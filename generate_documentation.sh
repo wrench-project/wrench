a094283ae41a4ae4b75d4135d7b4b41eff968012
Rafael Ferreira da Silva <rafsilva@isi.edu>
2017-05-12 08:14:57 -1000

    improving documentation auto-generation

--- generate_documentation.sh
+++ generate_documentation.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 CMD=""
+GH_DOCS=""
 
 while test $# -gt 0
 do
@@ -10,6 +11,8 @@ do
         --DEVELOPER) CMD="$CMD DEVELOPER"
             ;;
         --INTERNAL) CMD="$CMD INTERNAL"
+            ;;
+        --GH) GH_DOCS="GH_DOCS"
             ;;
         *) echo "Usage: $0 [--USER] [--DEVELOPER] [--INTERNAL]"
 	   exit 1
@@ -25,12 +28,12 @@ fi
 
 # WRENCH version
 VERSION=$(<./.version)
-mkdir -p docs/$VERSION
+mkdir -p docs/$VERSION/logs
 
 for ITEM in $CMD ; do
 	echo "Generating $ITEM doc..."
 	OUTPUT_DIRECTORY=$(echo $ITEM | tr '[:upper:]' '[:lower:]')
-	DOXYFILE="docs/$VERSION/Doxyfile_$ITEM"
+	DOXYFILE="docs/$VERSION/logs/Doxyfile_$ITEM"
 	SECTIONS=""
 	if [ $ITEM == DEVELOPER ] ; then
 	  SECTIONS="DEVELOPER"
@@ -39,9 +42,10 @@ for ITEM in $CMD ; do
 	  SECTIONS="DEVELOPER INTERNAL"
 	fi
 	cat ./Doxyfile.in | sed "s/WRENCH_SECTIONS/$SECTIONS/" | sed "s/WRENCH_OUTPUT_DIRECTORY/\docs\/$VERSION\/$OUTPUT_DIRECTORY/"> $DOXYFILE
-#	doxygen $DOXYFILE 1> /dev/null 2> /dev/null
-	doxygen $DOXYFILE
-	mkdir -p ./docs/gh-pages/$VERSION/$OUTPUT_DIRECTORY
-	cp -R ./docs/$VERSION/$OUTPUT_DIRECTORY/html/*  ./docs/gh-pages/$VERSION/$OUTPUT_DIRECTORY
+	doxygen $DOXYFILE &> $DOXYFILE.log
+	if [[ ! -z "$GH_DOCS" ]] ; then
+	  mkdir -p ./docs/gh-pages/$VERSION/$OUTPUT_DIRECTORY
+	  cp -R ./docs/$VERSION/$OUTPUT_DIRECTORY/html/*  ./docs/gh-pages/$VERSION/$OUTPUT_DIRECTORY
+	fi
 	echo "$ITEM doc generated at docs/$VERSION/$OUTPUT_DIRECTORY/html/index.html"
 done
