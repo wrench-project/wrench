01ba36ead829562fcd02dc15a21a6252bc657f21
henricasanova <henric@hawaii.edu>
2017-11-29 13:40:31 -1000

    Added all test for File Registry Service messages. Let's see if the coverage increases.

--- test/constructor_exceptions/MessageConstructorTest.cpp
+++ test/constructor_exceptions/MessageConstructorTest.cpp
@@ -18,19 +18,50 @@ protected:
       workflow = new wrench::Workflow();
       task = workflow->addTask("task", 1);
       file = workflow->addFile("file", 1);
+      storage_service = (wrench::StorageService *)(1234);
     }
 
     // data members
     wrench::Workflow *workflow;
     wrench::WorkflowTask *task;
     wrench::WorkflowFile *file;
+    wrench::StorageService *storage_service;
 };
 
 
+
+TEST_F(MessageConstructorTest, SimulationMessages) {
+
+  EXPECT_NO_THROW(new wrench::SimulationMessage("name", 666));
+  EXPECT_THROW(new wrench::SimulationMessage("", 666), std::invalid_argument);
+  EXPECT_THROW(new wrench::SimulationMessage("name", -1), std::invalid_argument);
+
+}
+
+
 TEST_F(MessageConstructorTest, FileRegistryMessages) {
+
   EXPECT_NO_THROW(new wrench::FileRegistryFileLookupRequestMessage("mailbox", file, 666));
   EXPECT_THROW(new wrench::FileRegistryFileLookupRequestMessage("", file, 666), std::invalid_argument);
   EXPECT_THROW(new wrench::FileRegistryFileLookupRequestMessage("mailbox", nullptr, 666), std::invalid_argument);
-  EXPECT_THROW(new wrench::FileRegistryFileLookupRequestMessage("mailbox", file, -1), std::invalid_argument);
+
+  EXPECT_NO_THROW(new wrench::FileRegistryFileLookupAnswerMessage(file, {}, 666));
+  EXPECT_THROW(new wrench::FileRegistryFileLookupAnswerMessage(nullptr, {}, 666), std::invalid_argument);
+
+  EXPECT_NO_THROW(new wrench::FileRegistryRemoveEntryRequestMessage("mailbox", file, storage_service, 666));
+  EXPECT_THROW(new wrench::FileRegistryRemoveEntryRequestMessage("", file, storage_service, 666), std::invalid_argument);
+  EXPECT_THROW(new wrench::FileRegistryRemoveEntryRequestMessage("mailbox", nullptr, storage_service, 666), std::invalid_argument);
+  EXPECT_THROW(new wrench::FileRegistryRemoveEntryRequestMessage("mailbox", file, nullptr, 666), std::invalid_argument);
+
+  EXPECT_NO_THROW(new wrench::FileRegistryRemoveEntryAnswerMessage(true, 666));
+
+  EXPECT_NO_THROW(new wrench::FileRegistryAddEntryRequestMessage("mailbox", file, storage_service, 666));
+  EXPECT_THROW(new wrench::FileRegistryAddEntryRequestMessage("", file, storage_service, 666), std::invalid_argument);
+  EXPECT_THROW(new wrench::FileRegistryAddEntryRequestMessage("mailbox", nullptr, storage_service, 666), std::invalid_argument);
+  EXPECT_THROW(new wrench::FileRegistryAddEntryRequestMessage("mailbox", file, nullptr, 666), std::invalid_argument);
+
+  EXPECT_NO_THROW(new wrench::FileRegistryAddEntryAnswerMessage(666));
 }
+
+
 
