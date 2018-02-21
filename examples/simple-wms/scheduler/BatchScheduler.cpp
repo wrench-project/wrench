d326543f95c041f2a0b465e629d66bf35a0c3355
henricasanova <henric@hawaii.edu>
2018-02-21 11:55:49 -1000

    Some re-factoring of the examples   - Schedulers now take no arguments to the constructor   - They just get the compute resources from the scheduleTasks() methods

--- examples/simple-wms/scheduler/BatchScheduler.cpp
+++ examples/simple-wms/scheduler/BatchScheduler.cpp
@@ -17,18 +17,8 @@ namespace wrench {
     /**
      * @brief Constructor
      *
-     * @param batch_service: a pointer to a batch service
-     * @param execution_hosts: list of physical execution hosts to run the batch job
-     * @param simulation: a pointer to the simulation object
-     *
-     * @throw std::runtime_error
      */
-    BatchScheduler::BatchScheduler(ComputeService *batch_service, Simulation *simulation) : simulation(simulation) {
-
-      if (typeid(batch_service) == typeid(BatchService)) {
-        throw std::runtime_error("The provided batch service is not a BatchService object.");
-      }
-      this->batch_service = batch_service;
+    BatchScheduler::BatchScheduler() {
     }
 
     /**
@@ -44,10 +34,16 @@ namespace wrench {
                                        std::map<std::string, std::vector<WorkflowTask *>> ready_tasks,
                                        const std::set<ComputeService *> &compute_services) {
 
-      if (compute_services.find(batch_service) == compute_services.end()) {
-        throw std::runtime_error("The default batch service is not listed as a compute service.");
+      // Check that the right compute_services is passed
+      if (compute_services.size() != 1) {
+        throw std::runtime_error("This example Batch Scheduler requires a single compute service");
       }
-      auto *cs = (BatchService *) this->batch_service;
+
+      ComputeService *compute_service = *compute_services.begin();
+      BatchService *batch_service;
+      if (not(batch_service = dynamic_cast<BatchService *>(compute_service))) {
+        throw std::runtime_error("This example Batch Scheduler can only handle a batch service");
+      }
 
       WRENCH_INFO("There are %ld ready tasks to schedule", ready_tasks.size());
 
@@ -59,7 +55,7 @@ namespace wrench {
         batch_job_args["-N"] = "1";
         batch_job_args["-t"] = "2000000"; //time in minutes
         batch_job_args["-c"] = "1"; //number of cores per node
-        job_manager->submitJob(job, cs, batch_job_args);
+        job_manager->submitJob(job, batch_service, batch_job_args);
       }
       WRENCH_INFO("Done with scheduling tasks as standard jobs");
     }
