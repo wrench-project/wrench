b57c87d079bcb63b0aa39ceb5c587d0bf11dd65a
henricasanova <henric@hawaii.edu>
2018-04-04 21:25:25 -1000

    tmp

--- include/wrench/workflow/execution_events/WorkflowExecutionEvent.h
+++ include/wrench/workflow/execution_events/WorkflowExecutionEvent.h
@@ -22,11 +22,12 @@ namespace wrench {
 
     class WorkflowFile;
 
-    class WorkflowJob;
+    class StandardJob;
+    class PilotJob;
 
     class ComputeService;
-
     class StorageService;
+    class FileRegistryService;
 
     class FailureCause;
 
@@ -45,38 +46,25 @@ namespace wrench {
         /** @brief Workflow execution event types */
         enum EventType {
             /** @brief An error type */
-            UNDEFINED,
+                    UNDEFINED,
             /** @brief A standard job successfully completed */
-            STANDARD_JOB_COMPLETION,
+                    STANDARD_JOB_COMPLETION,
             /** @brief A standard job failed */
-            STANDARD_JOB_FAILURE,
+                    STANDARD_JOB_FAILURE,
             /** @brief A pilot job started */
-            PILOT_JOB_START,
+                    PILOT_JOB_START,
             /** @brief A pilot job expired */
-            PILOT_JOB_EXPIRATION,
+                    PILOT_JOB_EXPIRATION,
             /** @brief A file copy operation completed */
-            FILE_COPY_COMPLETION,
+                    FILE_COPY_COMPLETION,
             /** @brief A file copy operation failed */
-            FILE_COPY_FAILURE,
+                    FILE_COPY_FAILURE,
         };
-
 
         /** @brief The event type */
         WorkflowExecutionEvent::EventType type;
-        /** @brief The relevant job, or nullptr */
-        WorkflowJob *job = nullptr;
-        /** @brief The relevant compute service, or nullptr */
-        ComputeService *compute_service = nullptr;
-        /** @brief The relevant file, or nullptr */
-        WorkflowFile *file = nullptr;
-        /** @brief The relevant storage service, or nullptr */
-        StorageService *storage_service = nullptr;
-        /** @brief The relevant failure cause, or nullptr */
-        std::shared_ptr<FailureCause> failure_cause = nullptr;
 
-
-    private:
-        WorkflowExecutionEvent();
+        WorkflowExecutionEvent(EventType type) : type(type) {}
 
         friend class Workflow;
 
@@ -84,9 +72,106 @@ namespace wrench {
 
     };
 
-    /***********************/
-    /** \endcond           */
-    /***********************/
+    class StandardJobCompletionEvent : public WorkflowExecutionEvent {
+
+    public:
+
+        StandardJobCompletionEvent(StandardJob *standard_job,
+                                   ComputeService *compute_service)
+                : WorkflowExecutionEvent(STANDARD_JOB_COMPLETION),
+                  standard_job(standard_job), compute_service(compute_service) {}
+
+        StandardJob *standard_job;
+        ComputeService *compute_service;
+    };
+
+    class StandardJobFailedEvent : public WorkflowExecutionEvent {
+
+    public:
+
+        StandardJobFailedEvent(StandardJob *standard_job,
+                               ComputeService *compute_service,
+                               std::shared_ptr<FailureCause> failure_cause)
+                : WorkflowExecutionEvent(STANDARD_JOB_FAILURE),
+                  standard_job(standard_job),
+                  compute_service(compute_service),
+                  failure_cause(failure_cause) {}
+
+        StandardJob *standard_job;
+        ComputeService *compute_service;
+        std::shared_ptr<FailureCause> failure_cause;
+    };
+
+
+    class PilotJobStartedEvent : public WorkflowExecutionEvent {
+
+    public:
+
+        PilotJobStartedEvent(PilotJob *pilot_job,
+                             ComputeService *compute_service)
+                : WorkflowExecutionEvent(PILOT_JOB_START),
+                  pilot_job(pilot_job), compute_service(compute_service) {}
+
+        PilotJob *pilot_job;
+        ComputeService *compute_service;
+    };
+
+    class PilotJobExpiredEvent : public WorkflowExecutionEvent {
+
+    public:
+
+        PilotJobExpiredEvent(PilotJob *pilot_job,
+                             ComputeService *compute_service)
+                : WorkflowExecutionEvent(PILOT_JOB_EXPIRATION),
+                  pilot_job(pilot_job), compute_service(compute_service) {}
+
+        PilotJob *pilot_job;
+        ComputeService *compute_service;
+    };
+
+    class FileCopyCompletedEvent : public WorkflowExecutionEvent {
+
+    public:
+
+        FileCopyCompletedEvent(WorkflowFile *file,
+                               StorageService *storage_service)
+                : WorkflowExecutionEvent(FILE_COPY_COMPLETION),
+                  file(file), storage_service(storage_service) {}
+
+        WorkflowFile *file;
+        StorageService *storage_service;
+    };
+
+
+    class FileCopyFailedEvent : public WorkflowExecutionEvent {
+
+    public:
+
+        FileCopyFailedEvent(WorkflowFile *file,
+                            StorageService *storage_service,
+                            FileRegistryService *file_registry_service,
+                            bool file_registry_service_updated,
+                            std::shared_ptr<FailureCause> failure_cause
+        )
+                : WorkflowExecutionEvent(FILE_COPY_COMPLETION),
+                  file(file), storage_service(storage_service),
+                  file_registry_service(file_registry_service),
+                  file_registry_service_updated(file_registry_service_updated) {}
+
+        WorkflowFile *file;
+        StorageService *storage_service;
+        FileRegistryService *file_registry_service;
+        bool file_registry_service_updated;
+        std::shared_ptr<FailureCause> failure_cause;
+
+    };
+
+
+
+
+/***********************/
+/** \endcond           */
+/***********************/
 
 };
 
