de305281df53e389235b7c969033bbe5f7aee55e
henricasanova <henric@hawaii.edu>
2018-06-01 18:51:17 +0200

    TODO -= 2 bug fix in OneJobWMS (had to call addWorkflow() within OneJobWMS::main()!)

--- include/wrench/services/network_proximity/NetworkProximityDaemon.h
+++ include/wrench/services/network_proximity/NetworkProximityDaemon.h
@@ -10,6 +10,7 @@
 #ifndef WRENCH_NETWORKDAEMONS_H
 #define WRENCH_NETWORKDAEMONS_H
 
+#include <random>
 #include "wrench/services/Service.h"
 #include "wrench/services/network_proximity/NetworkProximityServiceProperty.h"
 #include "wrench/services/network_proximity/NetworkProximityServiceMessagePayload.h"
@@ -60,6 +61,9 @@ namespace wrench {
         double message_size;
         double measurement_period;
         double max_noise;
+        std::default_random_engine rng;
+        std::uniform_real_distribution<double> noise_dist;
+
         std::string suffix;
         std::string next_mailbox_to_send;
         std::string next_host_to_send;
