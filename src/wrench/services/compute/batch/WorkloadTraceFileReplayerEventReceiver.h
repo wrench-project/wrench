a3093ae3f387c76ff0b76956715c7975a33d5172
henricasanova <henric@hawaii.edu>
2018-08-10 21:40:43 -1000

    tmp

---
+++ src/wrench/services/compute/batch/WorkloadTraceFileReplayerEventReceiver.h
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2017. The WRENCH Team.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+#ifndef WRENCH_ONEJOBWMS_H
+#define WRENCH_ONEJOBWMS_H
+
+#include "wrench/wms/WMS.h"
+
+/***********************/
+/** \cond INTERNAL    **/
+/***********************/
+
+namespace wrench {
+
+    class BatchService;
+
+    /**
+     * @brief A WMS that only submits a single job to a given batch service, which is used
+     *        to implement batch workload replay
+     */
+    class WorkloadTraceFileReplayerEventReceiver : public WMS {
+
+    public:
+        /**
+         * @brief Constructor
+         * @param hostname: the name of the host on which the "one job" WMS will run
+         */
+        WorkloadTraceFileReplayerEventReceiver(std::string hostname, std::shared_ptr<JobManager> job_manager) : WMS(nullptr, nullptr,
+                                                                               {}, {},
+                                                                               {},
+                                                                               nullptr, hostname,
+                                                                               "workload_trace_file_replayer_event_receiver"),
+                                                                           job_manager(job_manager) {}
+        int main() override;
+
+    private:
+        std::string hostname;
+        std::shared_ptr<JobManager> job_manager;
+    };
+
+};
+
+/***********************/
+/** \endcond          **/
+/***********************/
+
+#endif //WRENCH_ONEJOBWMS_H
