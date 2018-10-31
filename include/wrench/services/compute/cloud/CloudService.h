8ace47964fc2ad884f106eb4d0e50c56347d3646
henricasanova <henric@hawaii.edu>
2018-10-30 15:38:40 -1000

    Removed PilotJob Support from CloudService

--- include/wrench/services/compute/cloud/CloudService.h
+++ include/wrench/services/compute/cloud/CloudService.h
@@ -38,7 +38,7 @@ namespace wrench {
 
     private:
         std::map<std::string, std::string> default_property_values = {
-                {CloudServiceProperty::SUPPORTS_PILOT_JOBS,         "true"},
+                {CloudServiceProperty::SUPPORTS_PILOT_JOBS,         "false"},
                 {CloudServiceProperty::SUPPORTS_STANDARD_JOBS,      "true"},
                 {CloudServiceProperty::VM_BOOT_OVERHEAD_IN_SECONDS, "0.0"}
         };
@@ -105,7 +105,7 @@ namespace wrench {
 
         void terminateStandardJob(StandardJob *job) override;
 
-        void terminatePilotJob(PilotJob *job) override;
+        void validateProperties();
 
         ~CloudService();
 
