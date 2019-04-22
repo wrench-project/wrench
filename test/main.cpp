8647a30d3b594aba1edb78dbe6acbfb68c623270
henricasanova <henric@hawaii.edu>
2019-04-21 23:27:37 -1000

    bug-- test++

--- test/main.cpp
+++ test/main.cpp
@@ -12,13 +12,13 @@
 
 int main(int argc, char **argv) {
   // disable log
-//   xbt_log_control_set("root.thresh:critical");
+   xbt_log_control_set("root.thresh:critical");
 
 //   xbt_log_control_set("simulation_timestamps.thresh:debug");
 
 //   Example selective log enabling
 //   xbt_log_control_set("mailbox.thresh:debug");
-   xbt_log_control_set("comprehensive_failure_integration_test.thresh:info");
+//   xbt_log_control_set("comprehensive_failure_integration_test.thresh:info");
 //   xbt_log_control_set("s4u_daemon.thresh:info");
 //   xbt_log_control_set("host_random_repeat_switcher.thresh:info");
 //   xbt_log_control_set("simple_storage_service.thresh:info");
