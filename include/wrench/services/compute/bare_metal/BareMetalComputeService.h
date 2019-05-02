807933e7f222e054e4a57719d5f31b9a83bafcdf
henricasanova <henric@hawaii.edu>
2019-05-01 20:20:04 -1000

    fix

--- include/wrench/services/compute/bare_metal/BareMetalComputeService.h
+++ include/wrench/services/compute/bare_metal/BareMetalComputeService.h
@@ -196,8 +196,6 @@ namespace wrench {
 
         void failCurrentStandardJobs();
 
-        bool processMessage(SimulationMessage *msh);
-
         void processWorkunitExecutorCompletion(WorkunitExecutor *workunit_executor, std::shared_ptr<Workunit> workunit);
 
         void processWorkunitExecutorFailure(WorkunitExecutor *workunit_executor, std::shared_ptr<Workunit> workunit, std::shared_ptr<FailureCause> cause);
