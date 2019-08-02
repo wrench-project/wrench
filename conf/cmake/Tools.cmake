42461ce17d1cc9c4da2daf267f2dd2beee491ff5
henricasanova <henric@hawaii.edu>
2019-08-01 23:04:17 -1000

    Have taken the Pegasus workflow import (from DAX/JSON) out of the wrench core and put it in tools/pegasus

---
+++ conf/cmake/Tools.cmake
@@ -0,0 +1,19 @@
+# install wrench-init tool
+install(FILES ${CMAKE_CURRENT_SOURCE_DIR}/tools/wrench/wrench-init
+        DESTINATION bin
+        PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE)
+
+# compile/insrall the pegasus workflow parser
+set(PEGASUS_WORKFLOW_PARSER_SOURCE_FILES
+        tools/pegasus/src/PegasusWorkflowParser.cpp
+        )
+
+set(PEGASUS_WORKFLOW_PARSER_HEADER_FILES
+        tools/pegasus/include/PegasusWorkflowParser.h
+        )
+
+add_library(wrenchpegasusworkflowparser STATIC ${PEGASUS_WORKFLOW_PARSER_SOURCE_FILES} )
+install(TARGETS wrenchpegasusworkflowparser DESTINATION lib)
+install(FILES "${PEGASUS_WORKFLOW_PARSER_HEADER_FILES}"
+        DESTINATION include/wrench-tools/
+        )
