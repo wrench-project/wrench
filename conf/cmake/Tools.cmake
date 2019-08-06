476b6e7b3f30cab49593251247b46e070467179d
henricasanova <henric@hawaii.edu>
2019-08-06 16:36:21 -0700

    tiny fix

--- conf/cmake/Tools.cmake
+++ conf/cmake/Tools.cmake
@@ -9,7 +9,7 @@ set(PEGASUS_WORKFLOW_PARSER_SOURCE_FILES
         )
 
 set(PEGASUS_WORKFLOW_PARSER_HEADER_FILES
-        tools/pegasus/include/PegasusWorkflowParser.h
+        include/tools/pegasus/PegasusWorkflowParser.h
         )
 
 add_library(wrenchpegasusworkflowparser STATIC ${PEGASUS_WORKFLOW_PARSER_SOURCE_FILES} )
