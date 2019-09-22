77e53f3721a1847a567d86df8d8140855e16e502
henricasanova <henric@hawaii.edu>
2019-09-21 23:08:14 -1000

    Broke  everything :)

---
+++ src/wrench/services/storage/storage_helpers/FileLocation.cpp
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2017. The WRENCH Team.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+#include <memory>
+#include <wrench/services/storage/StorageService.h>
+#include <wrench/services/storage/storage_helpers/FileLocation.h>
+
+
+namespace wrench {
+
+    /**
+     * @brief File location specifier (directory "/")
+     *
+     * @param ss: a storage service (that has a single mount point)
+     * @return a file location specification
+     *
+     * @throw std::invalid_argument
+     */
+    std::shared_ptr<FileLocation> FileLocation::LOCATION(std::shared_ptr<StorageService> ss) {
+        if (ss == nullptr) {
+            throw std::invalid_argument("FileLocation::LOCATION(): Cannot pass nullptr storage service");
+        }
+        if (ss->hasMultipleMountPoints()) {
+            throw std::invalid_argument("FileLocation::LOCATION(): Storage Service has multiple mount points. "
+                                        "Call the version of this method that takes a mount point argument");
+        }
+        return LOCATION(ss, *(ss->getMountPoints().begin()), "/");
+    }
+
+    /**
+     * @brief File location specifier (directory = "/")
+     *
+     * @param ss: a storage service
+     * @param mp: a mount point of the storage service
+     * @return a file location specification
+     *
+     * @throw std::invalid_argument
+     */
+    std::shared_ptr<FileLocation> FileLocation::LOCATION(std::shared_ptr<StorageService> ss,
+                                                         std::string mp) {
+        if (ss == nullptr) {
+            throw std::invalid_argument("FileLocation::LOCATION(): Cannot pass nullptr storage service");
+        }
+
+        return LOCATION(ss, mp, "/");
+    }
+
+    /**
+     * @brief File location specifier
+     *
+     * @param ss: a storage service
+     * @param mp: a mount point of the storage service
+     * @param dir: a directory
+     * @return a file location specification
+     *
+     * @throw std::invalid_argument
+     */
+    std::shared_ptr<FileLocation> FileLocation::LOCATION(std::shared_ptr<StorageService> ss,
+                                                         std::string mp,
+                                                         std::string dir) {
+        if (ss == nullptr) {
+            throw std::invalid_argument("FileLocation::LOCATION(): Cannot pass nullptr storage service");
+        }
+        if (not ss->hasMountPoint(mp)) {
+            throw std::invalid_argument("FileLocation::LOCATION(): Storage service " + ss->getName() +
+                                        " does not have mount point " + mp);
+        }
+        return std::shared_ptr<FileLocation>(new FileLocation(ss, mp, dir));
+    }
+
+    /**
+     * @brief Give a <ss name>:<mount point>:<dir>" string for the location
+     */
+    std::string FileLocation::toString() {
+        return this->storage_service->getName() + ":" + this->mount_point + ":" + this->directory;
+    }
+
+
+}
+
---
+++ src/wrench/services/storage/storage_helpers/LocicalFileSystem.cpp
@@ -0,0 +1,153 @@
+/**
+ * Copyright (c) 2017. The WRENCH Team.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+#include <wrench-dev.h>
+#include "LogicalFileSystem.h"
+
+namespace wrench {
+
+
+    std::set<std::string> mount_points;
+
+
+    /**
+     * @brief Constructor
+     * @param mount_point: the mount point
+     */
+    LogicalFileSystem::LogicalFileSystem(std::string hostname, std::string mount_point) {
+
+        // Check uniqueness
+        if (LogicalFileSystem::mount_points.find(hostname+":"+mount_point) != LogicalFileSystem::mount_points.end()) {
+            throw std::invalid_argument("LogicalFileSystem::LogicalFileSystem(): A FileSystem with mount point " +
+            mount_point + " at host " + hostname + " already exists");
+        }
+        LogicalFileSystem::mount_points.insert(hostname+":"+mount_point);
+
+        this->mount_point = mount_point;
+        this->content["/"] = {};
+        this->total_capacity = S4U_Simulation::getDiskCapacity(hostname, mount_point);
+        this->occupied_space = 0;
+    }
+
+
+    /**
+     * @brief Create a new directory
+     *
+     * @param absolute_path: the directory's absolute path
+     * @throw std::invalid_argument
+     */
+    void LogicalFileSystem::createDirectory(std::string absolute_path) {
+        assertDirectoryDoesNotExist(absolute_path);
+        this->content[absolute_path] = {};
+    }
+
+    /**
+     * @brief Checks whether a directory exists
+     * @param absolute_path the directory's absolute path
+     * @return true if the directory exists
+     */
+    bool LogicalFileSystem::doesDirectoryExist(std::string absolute_path) {
+        return (this->content.find(absolute_path) != this->content.end());
+    }
+
+    /**
+     * @brief Checks whether a directory is empty
+     * @param absolute_path: the directory's absolute path
+     * @return true if the directory is empty
+     *
+     * @throw std::invalid_argument
+     */
+    bool LogicalFileSystem::isDirectoryEmpty(std::string absolute_path) {
+        assertDirectoryExist(absolute_path);
+        return (this->content[absolute_path].empty());
+    }
+
+    /**
+     * @brief Remove an empty directory
+     * @param absolute_path: the directory's absolute path
+     *
+     * @throw std::invalid_argument
+     */
+    void LogicalFileSystem::removeEmptyDirectory(std::string absolute_path) {
+        assertDirectoryExist(absolute_path);
+        assertDirectoryIsEmpty(absolute_path);
+        this->content.erase(absolute_path);
+    }
+
+    /**
+     * @brief Store file in directory
+     * @param absolute_path: the directory's absolute path
+     *
+     * @throw std::invalid_argument
+     */
+    void LogicalFileSystem::storeFileInDirectory(WorkflowFile *file, std::string absolute_path) {
+        assertDirectoryExist(absolute_path);
+        this->content[absolute_path].insert(file);
+    }
+
+    /**
+     * @brief Remove a file in a directory
+     * @param file: the file to remove
+     * @param absolute_path: the directory's absolute path
+     *
+     * @throw std::invalid_argument
+     */
+    void LogicalFileSystem::removeFileFromDirectory(WorkflowFile *file, std::string absolute_path) {
+        assertDirectoryExist(absolute_path);
+        assertFileIsInDirectory(file, absolute_path);
+    }
+
+    /**
+     * @brief Remove all files in a directory
+     * @param absolute_path: the directory's absolute path
+     *
+     * @throw std::invalid_argument
+     */
+    void LogicalFileSystem::removeAllFilesInDirectory(std::string absolute_path) {
+        assertDirectoryExist(absolute_path);
+        this->content[absolute_path].clear();
+    }
+
+    /**
+     * @brief Checks whether a file is in a directory
+     * @param file: the file
+     * @param absolute_path: the directory's absolute path
+     *
+     * @return true if the file is present
+     *
+     * @throw std::invalid_argument
+     */
+    bool LogicalFileSystem::isFileInDirectory(WorkflowFile *file, std::string absolute_path) {
+        assertDirectoryExist(absolute_path);
+        return (this->content[absolute_path].find(file) != this->content[absolute_path].end());
+    }
+
+    /**
+     * @brief Get the files in a directory as a set
+     * @param absolute_path: the directory's absolute path
+     *
+     * @return a set of files
+     *
+     * @throw std::invalid_argument
+     */
+    std::set<WorkflowFile *> LogicalFileSystem::listFilesInDirectory(std::string absolute_path) {
+        assertDirectoryExist(absolute_path);
+        return this->content[absolute_path];
+    }
+
+    /**
+     * @brief Get the total capacity
+     * @return the total capacity
+     */
+    double LogicalFileSystem::getTotalCapacity() {
+        return this->total_capacity;
+    }
+
+
+}
