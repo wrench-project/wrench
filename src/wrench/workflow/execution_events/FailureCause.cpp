4af7589839c6e6259578e907fd563d20e4849638
henricasanova <henric@hawaii.edu>
2019-09-28 22:36:45 -1000

    test fix (#2)

--- src/wrench/workflow/execution_events/FailureCause.cpp
+++ src/wrench/workflow/execution_events/FailureCause.cpp
@@ -588,4 +588,36 @@ namespace wrench {
         }
     }
 
+    /**
+     * @brief Constructor
+     * @param storage_service: the storage service at which the mount point was unknown
+     * @param invalid_path: the invalid path
+     */
+    InvalidDirectoryPath::InvalidDirectoryPath(std::shared_ptr<StorageService> storage_service,
+                                               std::string invalid_path) {
+        this->storage_service = storage_service;
+        this->invalid_path = invalid_path;
+    }
+
+    /**
+     * @brief Get the storage service at which the path was invalid
+     * @return a storage service
+     */
+    std::shared_ptr<StorageService> InvalidDirectoryPath::getStorageService() {
+        return this->storage_service;
+    }
+
+    /**
+     * @brief Get the invalid path
+     * @return a path
+     */
+    std::string InvalidDirectoryPath::getInvalidPath() {
+        return this->invalid_path;
+    }
+
+    std::string InvalidDirectoryPath::toString() {
+        return "Storage service " + this->storage_service->getName() + " doesn't have a " +
+               this->getInvalidPath() + " path";
+    }
+
 };
