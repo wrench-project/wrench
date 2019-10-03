7d72fe8eff5d4c03708a91365e460c41bda2d40e
henricasanova <henric@hawaii.edu>
2019-10-02 16:43:54 -1000

    storage service performance tests passing (#2)

--- test/storage_services/SimpleStorageService/SimpleStorageServicePerformanceTest.cpp
+++ test/storage_services/SimpleStorageService/SimpleStorageServicePerformanceTest.cpp
@@ -20,8 +20,8 @@
 XBT_LOG_NEW_DEFAULT_CATEGORY(simple_storage_service_performance_test, "Log category for SimpleStorageServicePerformanceTest");
 
 
-#define FILE_SIZE 10000000000.00
-#define MBPS_BANDWIDTH 100
+#define FILE_SIZE (10*1000*1000*1000.00) // 10 GB
+#define MBPS_BANDWIDTH 100               // 100 MB
 #define STORAGE_SIZE (100.0 * FILE_SIZE)
 
 class SimpleStorageServicePerformanceTest : public ::testing::Test {
@@ -36,9 +36,10 @@ public:
     std::shared_ptr<wrench::ComputeService> compute_service = nullptr;
 
     void do_FileRead_test(double buffer_size);
-    void do_ConcurrentFileCopies_test();
+    void do_ConcurrentFileCopies_test(double buffer_size);
 
-    static double computeExpectedPipelineTime(double bw_stage1, double bw_stage2, double total_size, double buffer_size) {
+    static double computeExpectedTwoStagePipelineTime(double bw_stage1, double bw_stage2, double total_size,
+                                                      double buffer_size) {
         double expected_elapsed = 0;
         if (buffer_size >= total_size) {
             return (total_size / bw_stage1) + (total_size / bw_stage2);
@@ -49,6 +50,26 @@ public:
                    (num_full_buffers - 1) * (buffer_size / bottleneck_bandwidth) +
                    buffer_size / bw_stage2 +
                    (total_size - num_full_buffers * buffer_size) / bw_stage2;
+        }
+    }
+
+    static double computeExpectedThreeStagePipelineTime(double bw_stage1, double bw_stage2, double bw_stage3,
+                                                        double total_size, double buffer_size) {
+        double expected_elapsed = 0;
+        if (buffer_size >= total_size) {
+            return (total_size / bw_stage1) + (total_size / bw_stage2) + (total_size / bw_stage3);
+        } else {
+            double bottleneck_bandwidth = std::min<double>(std::min<double>(bw_stage1, bw_stage2), bw_stage3);
+            double num_full_buffers = std::floor(total_size / buffer_size);
+            double last_nonfull_buffer_size = total_size - num_full_buffers * buffer_size;
+
+            return buffer_size / bw_stage1 +
+                   buffer_size / std::min<double>(bw_stage1, bw_stage2) +
+                   (num_full_buffers - 2) * (buffer_size / bottleneck_bandwidth) +
+                   std::max<double>(last_nonfull_buffer_size / bw_stage1,
+                   buffer_size / std::min<double>(bw_stage2, bw_stage3)) +
+                   std::max<double>(last_nonfull_buffer_size / bw_stage2, buffer_size / bw_stage3) +
+                   last_nonfull_buffer_size / bw_stage3;
         }
     }
 
@@ -72,16 +93,16 @@ protected:
                           "   <zone id=\"AS0\" routing=\"Full\"> "
                           "       <host id=\"SrcHost\" speed=\"1f\"> "
                           "          <disk id=\"large_disk\" read_bw=\"" + std::to_string(MBPS_BANDWIDTH) +
-                          "MBps\" write_bw=\"40MBps\">"
-                          "             <prop id=\"size\" value=\"" + std::to_string(STORAGE_SIZE) +
+                          "MBps\" write_bw=\"" + std::to_string(MBPS_BANDWIDTH) + "MBps\">"
+                                                                                  "             <prop id=\"size\" value=\"" + std::to_string(STORAGE_SIZE) +
                           "B\"/>"
                           "             <prop id=\"mount\" value=\"/\"/>"
                           "          </disk>"
                           "       </host>"
                           "       <host id=\"DstHost\" speed=\"1f\"> "
                           "          <disk id=\"large_disk\" read_bw=\"" + std::to_string(MBPS_BANDWIDTH) +
-                          "MBps\" write_bw=\"40MBps\">"
-                          "             <prop id=\"size\" value=\"" + std::to_string(STORAGE_SIZE) +
+                          "MBps\" write_bw=\"" + std::to_string(MBPS_BANDWIDTH) + "MBps\">"
+                                                                                  "             <prop id=\"size\" value=\"" + std::to_string(STORAGE_SIZE) +
                           "B\"/>"
                           "             <prop id=\"mount\" value=\"/\"/>"
                           "          </disk>"
@@ -123,14 +144,17 @@ public:
     SimpleStorageServiceConcurrentFileCopiesTestWMS(SimpleStorageServicePerformanceTest *test,
                                                     const std::set<std::shared_ptr<wrench::ComputeService>> compute_services,
                                                     const std::set<std::shared_ptr<wrench::StorageService>> &storage_services,
-                                                    std::string hostname) :
+                                                    std::string hostname,
+                                                    double buffer_size) :
             wrench::WMS(nullptr, nullptr, compute_services, storage_services, {}, nullptr, hostname, "test") {
         this->test = test;
+        this->buffer_size = buffer_size;
     }
 
 private:
 
     SimpleStorageServicePerformanceTest *test;
+    double buffer_size;
 
     int main() {
 
@@ -139,7 +163,7 @@ private:
 
         auto file_registry_service = this->getAvailableFileRegistryService();
 
-
+#if 1
         // Time the time it takes to transfer a file from Src to Dst
         double copy1_start = wrench::Simulation::getCurrentSimulatedDate();
         data_movement_manager->initiateAsynchronousFileCopy(this->test->file_1,
@@ -149,6 +173,7 @@ private:
         std::shared_ptr<wrench::WorkflowExecutionEvent> event1 = this->getWorkflow()->waitForNextExecutionEvent();
         double event1_arrival = wrench::Simulation::getCurrentSimulatedDate();
 
+#endif
         // Now do 2 of them in parallel
         double copy2_start = wrench::Simulation::getCurrentSimulatedDate();
         data_movement_manager->initiateAsynchronousFileCopy(this->test->file_2,
@@ -167,8 +192,12 @@ private:
         std::shared_ptr<wrench::WorkflowExecutionEvent> event3 = this->getWorkflow()->waitForNextExecutionEvent();
         double event3_arrival = wrench::Simulation::getCurrentSimulatedDate();
 
+#if 1
+
         double transfer_time_1 = event1_arrival - copy1_start;
+#endif
         double transfer_time_2 = event2_arrival - copy2_start;
+#if 1
         double transfer_time_3 = event3_arrival - copy3_start;
 
         // Do relative checks
@@ -185,16 +214,33 @@ private:
             throw std::runtime_error("Time between two asynchronous operation completions is too big");
         }
 
+#endif
 
-        // Do absolute checks
-        double expected_transfer_time_1 =  FILE_SIZE / (0.92 * MBPS_BANDWIDTH*1000*1000);
+
+#if 1
+        // Do absolute checksThree
+        double expected_transfer_time_1 =
+                SimpleStorageServicePerformanceTest::computeExpectedThreeStagePipelineTime(
+                        MBPS_BANDWIDTH * 1000 * 1000,
+                        0.92 * MBPS_BANDWIDTH * 1000 * 1000,
+                        MBPS_BANDWIDTH * 1000 * 1000,
+                        FILE_SIZE, buffer_size
+                );
 
         if (std::abs(transfer_time_1 - expected_transfer_time_1) > 1.0) {
             throw std::runtime_error("Unexpected transfer time #1 " + std::to_string(transfer_time_1) +
                                      " (should be around " + std::to_string(expected_transfer_time_1) + ")");
         }
+#endif
 
-        double expected_transfer_time_2 =  FILE_SIZE / (0.5 * 0.92 * MBPS_BANDWIDTH*1000*1000);
+
+        double expected_transfer_time_2 =
+                SimpleStorageServicePerformanceTest::computeExpectedThreeStagePipelineTime(
+                        MBPS_BANDWIDTH * 1000 * 1000 / 2,
+                        0.92 * MBPS_BANDWIDTH * 1000 * 1000 / 2,
+                        MBPS_BANDWIDTH * 1000 * 1000 / 2,
+                        FILE_SIZE, buffer_size
+                );
 
         if (std::abs(transfer_time_2 - expected_transfer_time_2) > 1.0) {
             throw std::runtime_error("Unexpected transfer time #2 " + std::to_string(transfer_time_2) +
@@ -206,10 +252,17 @@ private:
 };
 
 TEST_F(SimpleStorageServicePerformanceTest, ConcurrentFileCopies) {
-    DO_TEST_WITH_FORK(do_ConcurrentFileCopies_test);
+    DO_TEST_WITH_FORK_ONE_ARG(do_ConcurrentFileCopies_test, DBL_MAX);
+    DO_TEST_WITH_FORK_ONE_ARG(do_ConcurrentFileCopies_test, FILE_SIZE/2);
+    DO_TEST_WITH_FORK_ONE_ARG(do_ConcurrentFileCopies_test, FILE_SIZE/10);
+    DO_TEST_WITH_FORK_ONE_ARG(do_ConcurrentFileCopies_test, FILE_SIZE/10 + FILE_SIZE / 3);
+    DO_TEST_WITH_FORK_ONE_ARG(do_ConcurrentFileCopies_test, FILE_SIZE/20);
+    DO_TEST_WITH_FORK_ONE_ARG(do_ConcurrentFileCopies_test, FILE_SIZE/50);
+    DO_TEST_WITH_FORK_ONE_ARG(do_ConcurrentFileCopies_test, FILE_SIZE/100);
+    DO_TEST_WITH_FORK_ONE_ARG(do_ConcurrentFileCopies_test, FILE_SIZE/200);
 }
 
-void SimpleStorageServicePerformanceTest::do_ConcurrentFileCopies_test() {
+void SimpleStorageServicePerformanceTest::do_ConcurrentFileCopies_test(double buffer_size) {
 
     // Create and initialize a simulation
     auto simulation = new wrench::Simulation();
@@ -229,16 +282,18 @@ void SimpleStorageServicePerformanceTest::do_ConcurrentFileCopies_test() {
 
     // Create Two Storage Services
     ASSERT_NO_THROW(storage_service_1 = simulation->add(
-            new wrench::SimpleStorageService("SrcHost", {"/"})));
+            new wrench::SimpleStorageService("SrcHost", {"/"},
+                                             {{wrench::StorageServiceProperty::BUFFER_SIZE, std::to_string(buffer_size)}})));
     ASSERT_NO_THROW(storage_service_2 = simulation->add(
-            new wrench::SimpleStorageService("DstHost", {"/"})));
+            new wrench::SimpleStorageService("DstHost", {"/"},
+                                             {{wrench::StorageServiceProperty::BUFFER_SIZE, std::to_string(buffer_size)}})));
 
     // Create a WMS
     std::shared_ptr<wrench::WMS> wms = nullptr;
     ASSERT_NO_THROW(wms = simulation->add(
             new SimpleStorageServiceConcurrentFileCopiesTestWMS(
                     this, {compute_service}, {storage_service_1, storage_service_2},
-                    "WMSHost")));
+                    "WMSHost", buffer_size)));
 
     wms->addWorkflow(this->workflow);
 
@@ -299,7 +354,7 @@ private:
         double effective_network_bandwidth = (MBPS_BANDWIDTH * 1000 * 1000) * 0.92;
         double effective_disk_bandwidth = (MBPS_BANDWIDTH * 1000 * 1000);
 
-        double expected_elapsed = SimpleStorageServicePerformanceTest::computeExpectedPipelineTime(
+        double expected_elapsed = SimpleStorageServicePerformanceTest::computeExpectedTwoStagePipelineTime(
                 effective_disk_bandwidth, effective_network_bandwidth, FILE_SIZE, buffer_size);
 
         if (std::abs(elapsed - expected_elapsed) > 1.0) {
@@ -317,6 +372,7 @@ TEST_F(SimpleStorageServicePerformanceTest, FileRead) {
     DO_TEST_WITH_FORK_ONE_ARG(do_FileRead_test, DBL_MAX);
     DO_TEST_WITH_FORK_ONE_ARG(do_FileRead_test, FILE_SIZE/2);
     DO_TEST_WITH_FORK_ONE_ARG(do_FileRead_test, FILE_SIZE/10);
+    DO_TEST_WITH_FORK_ONE_ARG(do_FileRead_test, FILE_SIZE/10 + FILE_SIZE / 3);
     DO_TEST_WITH_FORK_ONE_ARG(do_FileRead_test, FILE_SIZE/20);
     DO_TEST_WITH_FORK_ONE_ARG(do_FileRead_test, FILE_SIZE/50);
     DO_TEST_WITH_FORK_ONE_ARG(do_FileRead_test, FILE_SIZE/100);
