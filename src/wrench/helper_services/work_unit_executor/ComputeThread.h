1eb0606935787d7d4f28f4962592784319fc393a
henricasanova <henric@hawaii.edu>
2019-11-27 14:35:11 -1000

    Directory reorg

--- src/wrench/helper_services/work_unit_executor/ComputeThread.h
+++ src/wrench/helper_services/work_unit_executor/ComputeThread.h
@@ -13,7 +13,7 @@
 
 #include <wrench/simgrid_S4U_util/S4U_Mailbox.h>
 #include "wrench/simgrid_S4U_util/S4U_Simulation.h"
-#include "services/compute/standard_job_executor/StandardJobExecutorMessage.h"
+#include "helper_services/standard_job_executor/StandardJobExecutorMessage.h"
 
 
 namespace wrench {
