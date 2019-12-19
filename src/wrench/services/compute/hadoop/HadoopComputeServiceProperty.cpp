e0f77b27f5ac356316653a11e73df9af82e14b46
henricasanova <henric@hawaii.edu>
2019-12-19 10:05:18 -1000

    Started adding the skeleton of a Hadoop Compute Service

---
+++ src/wrench/services/compute/hadoop/HadoopComputeServiceProperty.cpp
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2017-2019. The WRENCH Team.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+#include "HadoopComputeServiceProperty.h"
+
+
+namespace wrench {
+
+    SET_PROPERTY_NAME(HadoopComputeServiceProperty, MAP_STARTUP_OVERHEAD);
+
+}
---
+++ src/wrench/services/compute/hadoop/HadoopComputeServiceProperty.h
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2017-2019. The WRENCH Team.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+#ifndef WRENCH_HADOOPCOMPUTESERVICEPROPERTY_H
+#define WRENCH_HADOOPCOMPUTESERVICEPROPERTY_H
+
+
+class HadoopComputeServiceProperty {
+
+#include "wrench/services/compute/ComputeServiceProperty.h"
+
+namespace wrench {
+
+    /**
+     * @brief Configurable properties for a HadoopComputeService
+     */
+    class HadoopComputeServiceProperty: public ComputeServiceProperty {
+
+    public:
+        /**
+         * @brief The overhead to start a map task (placeholder example property to be changed later)
+         */
+        DECLARE_PROPERTY_NAME(MAP_STARTUP_OVERHEAD);
+
+    }
+
+};
+
+
+#endif //WRENCH_HADOOPCOMPUTESERVICEPROPERTY_H
