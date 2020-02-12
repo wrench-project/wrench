58e43ce3470bf198cdfe71f77066e37da39ab657
henricasanova <henric@hawaii.edu>
2020-02-12 09:43:11 +0100

    started changing stuff

--- include/wrench/workflow/job/StandardJob.h
+++ include/wrench/workflow/job/StandardJob.h
@@ -76,13 +76,13 @@ namespace wrench {
         unsigned long num_completed_tasks;
         /** @brief The file locations that tasks should read/write files from/to */
         std::map<WorkflowFile *, std::shared_ptr<FileLocation>> file_locations;
-        /** @brief The file copy operations to perform before computational tasks */
-        std::set<std::tuple<WorkflowFile *, std::shared_ptr<FileLocation>  , std::shared_ptr<FileLocation>  >> pre_file_copies;
+        /** @brief The ordered file copy operations to perform before computational tasks */
+        std::vector<std::tuple<WorkflowFile *, std::shared_ptr<FileLocation>  , std::shared_ptr<FileLocation>  >> pre_file_copies;
 
-        /** @brief The file copy operations to perform after computational tasks */
-        std::set<std::tuple<WorkflowFile *, std::shared_ptr<FileLocation>  , std::shared_ptr<FileLocation>  >> post_file_copies;
-        /** @brief The file deletion operations to perform at the end */
-        std::set<std::tuple<WorkflowFile *, std::shared_ptr<FileLocation>  >> cleanup_file_deletions;
+        /** @brief The ordered file copy operations to perform after computational tasks */
+        std::vector<std::tuple<WorkflowFile *, std::shared_ptr<FileLocation>  , std::shared_ptr<FileLocation>  >> post_file_copies;
+        /** @brief The ordered file deletion operations to perform at the end */
+        std::vector<std::tuple<WorkflowFile *, std::shared_ptr<FileLocation>  >> cleanup_file_deletions;
 
     private:
 
@@ -95,9 +95,9 @@ namespace wrench {
 
         StandardJob(Workflow *workflow,
                     std::vector<WorkflowTask *> tasks, std::map<WorkflowFile *, std::shared_ptr<FileLocation>  > &file_locations,
-                    std::set<std::tuple<WorkflowFile *, std::shared_ptr<FileLocation>  , std::shared_ptr<FileLocation>  >> &pre_file_copies,
-                    std::set<std::tuple<WorkflowFile *, std::shared_ptr<FileLocation>  , std::shared_ptr<FileLocation>  >> &post_file_copies,
-                    std::set<std::tuple<WorkflowFile *, std::shared_ptr<FileLocation>  >> &cleanup_file_deletions);
+                    std::vector<std::tuple<WorkflowFile *, std::shared_ptr<FileLocation>  , std::shared_ptr<FileLocation>  >> &pre_file_copies,
+                    std::vector<std::tuple<WorkflowFile *, std::shared_ptr<FileLocation>  , std::shared_ptr<FileLocation>  >> &post_file_copies,
+                    std::vector<std::tuple<WorkflowFile *, std::shared_ptr<FileLocation>  >> &cleanup_file_deletions);
 
         State state;
 
