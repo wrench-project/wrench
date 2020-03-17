e996169c25ca0997a5eb81553acda25abce8f707
Rafael Ferreira da Silva <rafsilva@isi.edu>
2020-03-16 18:31:06 -0700

    installing dashboard scripts into /usr/local

--- conf/cmake/Tools.cmake
+++ conf/cmake/Tools.cmake
@@ -1,9 +1,17 @@
 # install wrench-init tool
 install(FILES ${CMAKE_CURRENT_SOURCE_DIR}/tools/wrench/wrench-init
         DESTINATION bin
-        PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE)
+        PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE
+        )
 
-# compile/insrall the pegasus workflow parser
+# install dashboard
+install(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/tools/wrench/dashboard
+        DESTINATION wrench
+        PATTERN "*"
+        PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE
+        )
+
+# compile/install the pegasus workflow parser
 set(PEGASUS_WORKFLOW_PARSER_SOURCE_FILES
         tools/pegasus/src/PegasusWorkflowParser.cpp
         )
@@ -12,7 +20,7 @@ set(PEGASUS_WORKFLOW_PARSER_HEADER_FILES
         include/wrench/tools/pegasus/PegasusWorkflowParser.h
         )
 
-add_library(wrenchpegasusworkflowparser STATIC ${PEGASUS_WORKFLOW_PARSER_SOURCE_FILES} )
+add_library(wrenchpegasusworkflowparser STATIC ${PEGASUS_WORKFLOW_PARSER_SOURCE_FILES})
 install(TARGETS wrenchpegasusworkflowparser DESTINATION lib)
 install(FILES "${PEGASUS_WORKFLOW_PARSER_HEADER_FILES}"
         DESTINATION include/wrench/tools/pegasus/
