53479a8c0c37e666b56c35c0fac40591c1e91338
Rafael Ferreira da Silva <rafsilva@isi.edu>
2020-06-17 19:30:22 -0700

    more improvements for the dashboard design

--- tools/wrench/dashboard/partials/render.js
+++ tools/wrench/dashboard/partials/render.js
@@ -2,42 +2,36 @@ const graphInfoArray = [
     {
         "title": "Simulation Metrics",
         "containerId": "overall-metrics-table-container",
-        "arrowId": "overall-metrics-arrow",
         "html": workflowSummaryHtml,
         "icon": "chart pie"
     },
     {
         "title": "Gantt Chart",
         "containerId": "overall-graph-container",
-        "arrowId": "simulation-graph-arrow",
         "html": simulationGraphHtml,
         "icon": "sitemap"
     },
     {
         "title": "Simulation Details",
         "containerId": "task-details-table-container",
-        "arrowId": "simulation-details-arrow",
         "html": simulationDetailsHtml,
         "icon": "table"
     },
     {
         "title": "Host Utilization",
         "containerId": "host-utilization-graph-container",
-        "arrowId": "host-utilization-arrow",
         "html": hostUtilizationHtml,
         "icon": "chart bar"
     },
     {
         "title": "Energy Consumption",
         "containerId": "energy-graph",
-        "arrowId": "energy-graph-arrow",
         "html": energyGraphsHtml,
         "icon": "lightbulb"
     },
     {
         "title": "3D Visualization",
         "containerId": "three-d-graph-container",
-        "arrowId": "three-d-graph-arrow",
         "html": threedVisualizationHtml,
         "icon": "cubes"
     }
@@ -52,8 +46,7 @@ function render() {
     document.getElementById('main-sidebar').innerHTML = renderGraphsMenu({graphInfo: graphInfoArray})
 }
 
-render()
+render();
 
 $('.ui.dropdown')
-    .dropdown()
-;
+    .dropdown();
