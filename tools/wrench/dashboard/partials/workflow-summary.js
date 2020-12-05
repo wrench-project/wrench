0255bd8c85a2af07bb88a513fdcda977f7f1f728
Rafael Ferreira da Silva <rafsilva@isi.edu>
2020-12-04 18:26:07 -0800

    several improvements to dashboard

--- tools/wrench/dashboard/partials/workflow-summary.js
+++ tools/wrench/dashboard/partials/workflow-summary.js
@@ -1,17 +1,4 @@
-const workflowSummaryInnerHtml = `
-    <colgroup>
-        <col span="1" class='overall-metrics-table-col read-col'></col>
-        <col span="1" class='overall-metrics-table-col write-col'></col>
-    </colgroup>
-    <thead>
-        <tr>
-            <th class='task-details-table-td'>Metric</th>
-            <th class='task-details-table-td'>Value</th>
-        </tr>
-    </thead>`
-
-const workflowSummaryHtml =  `
-    <table id='overall-metrics-table'>
-        ${workflowSummaryInnerHtml}
-    </table>
+const workflowSummaryHtml = `
+    <div class="ui horizontal very relaxed list" id="overall-metrics-table">
+    </div>
 `
