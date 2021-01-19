179755a8ffecfc53a071992b21e11e5fed350fab
Henri Casanova <henricasanova@gmail.com>
2021-01-19 13:10:04 -1000

    debugging

--- test/services/compute_services/BatchService/BatchServiceTraceFileTest.cpp
+++ test/services/compute_services/BatchService/BatchServiceTraceFileTest.cpp
@@ -197,7 +197,7 @@ void BatchServiceTest::do_BatchTraceFileReplayTest_test() {
 
     // Create and initialize a simulation
     auto simulation = new wrench::Simulation();
-    int argc = 2;
+    int argc = 1;
     auto argv = (char **) calloc(argc, sizeof(char *));
     argv[0] = strdup("unit_test");
 //    argv[1] = strdup("--wrench-full-log");
