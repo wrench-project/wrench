85e688ae2b402a51d5e49a26b8bf3c2b02abcbe9
Henri Casanova <henric@hawaii.edu>
2021-08-10 17:02:09 -0700

    Removed logging

--- test/services/compute_services/htcondor/HTCondorServiceTest.cpp
+++ test/services/compute_services/htcondor/HTCondorServiceTest.cpp
@@ -907,7 +907,7 @@ void HTCondorServiceTest::do_NoStandardJobSupportTest_test() {
     int argc = 1;
     auto argv = (char **) calloc(argc, sizeof(char *));
     argv[0] = strdup("unit_test");
-    argv[1] = strdup("--wrench-full-log");
+    //argv[1] = strdup("--wrench-full-log");
 
     ASSERT_NO_THROW(simulation->init(&argc, argv));
 
