89baa7ab4742a26081b3a56a60b4d77bb000572f
Henri Casanova <henric@hawaii.edu>
2021-10-26 20:56:49 -1000

    Better code reuse in tests

---
+++ src/wrench/action/CustomAction.cpp
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2017-2019. The WRENCH Team.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+#include <wrench/simulation/Simulation.h>
+#include <wrench/action/Action.h>
+#include <wrench/action/CustomAction.h>
+
+#include <utility>
+
+namespace wrench {
+
+    /**
+    * @brief Constructor
+    * @param name: the action's name (if empty, a unique name will be picked for you)
+    * @param job: the job this action belongs to
+    * @param lambda: a lambda that implements the action's execution
+    * @param lambda: a lambda that implements the action's termination (typically a no-op)
+    */
+    CustomAction::CustomAction(const std::string& name, std::shared_ptr<CompoundJob> job,
+                               const std::function<void (std::shared_ptr<ActionExecutor> action_executor, unsigned long num_threads, double ram_footprint)> &lambda_execute,
+                               const std::function<void (std::shared_ptr<ActionExecutor> action_executor)> &lambda_terminate) :
+            Action(name, "custom_", std::move(job)), lambda_execute(lambda_execute), lambda_terminate(lambda_terminate) {
+    }
+
+    /**
+     * @brief Method to execute the action
+     * @param action_executor: the executor that executes this action
+     * @param num_threads: number of threads to use
+     * @param ram_footprint: ram footprint necessary
+     */
+    void CustomAction::execute(std::shared_ptr<ActionExecutor> action_executor, unsigned long num_threads, double ram_footprint) {
+        this->lambda_execute(action_executor, num_threads, ram_footprint);
+    }
+
+    /**
+     * @brief Method to terminate the action
+     * @param action_executor: the executor that executes this action
+     */
+    void CustomAction::terminate(std::shared_ptr<ActionExecutor> action_executor) {
+        this->lambda_terminate(action_executor);
+    }
+
+}
---
+++ test/services/helper_services/action_executor/SleepActionExecutorTest.cpp
@@ -0,0 +1,209 @@
+/**
+ * Copyright (c) 2017. The WRENCH Team.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+#include <gtest/gtest.h>
+#include <wrench-dev.h>
+
+#include <wrench/action/SleepAction.h>
+#include <wrench/services/helper_services/action_executor/ActionExecutorMessage.h>
+#include <wrench/services/helper_services/action_executor/ActionExecutor.h>
+#include <wrench/job/CompoundJob.h>
+#include <wrench/failure_causes/HostError.h>
+
+#include "../../../include/TestWithFork.h"
+#include "../../../include/UniqueTmpPathPrefix.h"
+
+WRENCH_LOG_CATEGORY(sleep_action_executor_test, "Log category for SleepActionExecutorTest");
+
+#define EPSILON (std::numeric_limits<double>::epsilon())
+
+class SleepActionExecutorTest : public ::testing::Test {
+
+public:
+    wrench::Simulation *simulation;
+
+    void do_SleepActionExecutorSuccessTest_test();
+
+
+protected:
+    SleepActionExecutorTest() {
+
+        std::string xml = "<?xml version='1.0'?>"
+                          "<!DOCTYPE platform SYSTEM \"http://simgrid.gforge.inria.fr/simgrid/simgrid.dtd\">"
+                          "<platform version=\"4.1\"> "
+                          "   <zone id=\"AS0\" routing=\"Full\"> "
+                          "       <host id=\"Host1\" speed=\"1f\" core=\"10\"> "
+                          "          <disk id=\"large_disk1\" read_bw=\"100MBps\" write_bw=\"100MBps\">"
+                          "             <prop id=\"size\" value=\"100GB\"/>"
+                          "             <prop id=\"mount\" value=\"/disk1/\"/>"
+                          "          </disk>"
+                          "          <disk id=\"large_disk2\" read_bw=\"100MBps\" write_bw=\"100MBps\">"
+                          "             <prop id=\"size\" value=\"100GB\"/>"
+                          "             <prop id=\"mount\" value=\"/disk2/\"/>"
+                          "          </disk>"
+
+                          "          <disk id=\"scratch\" read_bw=\"100MBps\" write_bw=\"100MBps\">"
+                          "             <prop id=\"size\" value=\"100B\"/>"
+                          "             <prop id=\"mount\" value=\"/scratch\"/>"
+                          "          </disk>"
+                          "       </host>"
+                          "       <host id=\"Host2\" speed=\"1f\" core=\"10\"> "
+                          "          <disk id=\"large_disk\" read_bw=\"100MBps\" write_bw=\"100MBps\">"
+                          "             <prop id=\"size\" value=\"100GB\"/>"
+                          "             <prop id=\"mount\" value=\"/\"/>"
+                          "          </disk>"
+                          "          <disk id=\"scratch\" read_bw=\"100MBps\" write_bw=\"100MBps\">"
+                          "             <prop id=\"size\" value=\"100B\"/>"
+                          "             <prop id=\"mount\" value=\"/scratch\"/>"
+                          "          </disk>"
+                          "       </host>"
+                          "       <host id=\"Host3\" speed=\"1f\" core=\"10\"> "
+                          "          <disk id=\"large_disk\" read_bw=\"100MBps\" write_bw=\"100MBps\">"
+                          "             <prop id=\"size\" value=\"100GB\"/>"
+                          "             <prop id=\"mount\" value=\"/\"/>"
+                          "          </disk>"
+                          "          <disk id=\"scratch\" read_bw=\"100MBps\" write_bw=\"100MBps\">"
+                          "             <prop id=\"size\" value=\"100B\"/>"
+                          "             <prop id=\"mount\" value=\"/scratch\"/>"
+                          "          </disk>"
+                          "       </host>"
+                          "       <host id=\"Host4\" speed=\"1f\" core=\"10\">  "
+                          "          <disk id=\"large_disk\" read_bw=\"100MBps\" write_bw=\"100MBps\">"
+                          "             <prop id=\"size\" value=\"100GB\"/>"
+                          "             <prop id=\"mount\" value=\"/\"/>"
+                          "          </disk>"
+                          "          <disk id=\"scratch\" read_bw=\"100MBps\" write_bw=\"100MBps\">"
+                          "             <prop id=\"size\" value=\"100B\"/>"
+                          "             <prop id=\"mount\" value=\"/scratch\"/>"
+                          "          </disk>"
+                          "         <prop id=\"ram\" value=\"1024B\"/> "
+                          "       </host>  "
+                          "       <link id=\"1\" bandwidth=\"5000GBps\" latency=\"0us\"/>"
+                          "       <link id=\"2\" bandwidth=\"0.1MBps\" latency=\"10us\"/>"
+                          "       <route src=\"Host1\" dst=\"Host2\"> <link_ctn id=\"1\"/> </route>"
+                          "       <route src=\"Host3\" dst=\"Host4\"> <link_ctn id=\"2\"/> </route>"
+                          "       <route src=\"Host1\" dst=\"Host4\"> <link_ctn id=\"2\"/> </route>"
+                          "   </zone> "
+                          "</platform>";
+        // Create a four-host 10-core platform file
+        FILE *platform_file = fopen(platform_file_path.c_str(), "w");
+        fprintf(platform_file, "%s", xml.c_str());
+        fclose(platform_file);
+
+    }
+
+    std::string platform_file_path = UNIQUE_TMP_PATH_PREFIX + "platform.xml";
+    std::unique_ptr<wrench::Workflow> workflow;
+
+};
+
+
+/**********************************************************************/
+/**  DO SLEEP ACTION EXECUTOR SUCCESS TEST                           **/
+/**********************************************************************/
+
+
+class SleepActionExecutorSuccessTestWMS : public wrench::WMS {
+
+public:
+    SleepActionExecutorSuccessTestWMS(SleepActionExecutorTest *test,
+                                      std::string hostname) :
+            wrench::WMS(nullptr, nullptr, {}, {}, {}, nullptr, hostname, "test") {
+        this->test = test;
+    }
+
+
+private:
+
+    SleepActionExecutorTest *test;
+
+    int main() {
+
+        // Create a job manager
+        auto job_manager = this->createJobManager();
+
+        // Create a compound job
+        auto job = job_manager->createCompoundJob("");
+        // Add a sleep_action
+        auto sleep_action = job->addSleepAction("", 10.0);
+        // Create a sleep action executor
+        auto sleep_action_executor = std::shared_ptr<wrench::ActionExecutor>(
+                new wrench::ActionExecutor("Host2", 0, 0.0,this->mailbox_name, sleep_action));
+        // Start it
+        sleep_action_executor->simulation = this->simulation;
+        sleep_action_executor->start(sleep_action_executor, true, false);
+        // Wait for a message from it
+        std::shared_ptr<wrench::SimulationMessage> message;
+        try {
+            message = wrench::S4U_Mailbox::getMessage(this->mailbox_name);
+        } catch (std::shared_ptr<wrench::NetworkError> &cause) {
+            throw std::runtime_error("Network error while getting reply from Executor!" + cause->toString());
+        }
+
+        // Did we get the expected message?
+        auto msg = std::dynamic_pointer_cast<wrench::ActionExecutorDoneMessage>(message);
+        if (!msg) {
+            throw std::runtime_error("Unexpected '" + message->getName() + "' message");
+        }
+
+        // Is the start-date sensible?
+        if (sleep_action->getStartDate() < 0.0 or sleep_action->getStartDate() > EPSILON) {
+            throw std::runtime_error("Unexpected action start date: " + std::to_string(sleep_action->getEndDate()));
+        }
+
+        // Is the end-date sensible?
+        if (sleep_action->getEndDate() + EPSILON < 10.0 or sleep_action->getEndDate() > 10.0 + EPSILON) {
+            throw std::runtime_error("Unexpected action end date: " + std::to_string(sleep_action->getEndDate()));
+        }
+
+        // Is the state sensible?
+        if (sleep_action->getState() != wrench::Action::State::COMPLETED) {
+            throw std::runtime_error("Unexpected action state: " + sleep_action->getStateAsString());
+        }
+
+        return 0;
+    }
+};
+
+TEST_F(SleepActionExecutorTest, SuccessTest) {
+    DO_TEST_WITH_FORK(do_SleepActionExecutorSuccessTest_test);
+}
+
+
+void SleepActionExecutorTest::do_SleepActionExecutorSuccessTest_test() {
+
+    // Create and initialize a simulation
+    simulation = new wrench::Simulation();
+    int argc = 1;
+    char **argv = (char **) calloc(argc, sizeof(char *));
+    argv[0] = strdup("unit_test");
+
+    simulation->init(&argc, argv);
+
+    // Setting up the platform
+    ASSERT_NO_THROW(simulation->instantiatePlatform(platform_file_path));
+
+    // Create a WMS
+    std::shared_ptr<wrench::WMS> wms = nullptr;
+    ASSERT_NO_THROW(wms = simulation->add(
+            new SleepActionExecutorSuccessTestWMS(this, "Host1")));
+
+    ASSERT_NO_THROW(wms->addWorkflow(new wrench::Workflow()));
+
+    ASSERT_NO_THROW(simulation->launch());
+
+    delete simulation;
+    for (int i=0; i < argc; i++)
+        free(argv[i]);
+    free(argv);
+
+}
+
+
+
