7241ee1ba6a9475b887d009a77c9ad80bd289cda
Henri Casanova <henric@hawaii.edu>
2021-11-15 10:14:25 -1000

    bug--

--- src/wrench/managers/JobManager.cpp
+++ src/wrench/managers/JobManager.cpp
@@ -378,7 +378,7 @@ namespace wrench {
      * @throw std::invalid_argument
      */
     std::shared_ptr<PilotJob> JobManager::createPilotJob() {
-        auto job = std::shared_ptr<PilotJob>(new PilotJob());
+        auto job = std::shared_ptr<PilotJob>(new PilotJob(this->getSharedPtr<JobManager>()));
         return job;
     }
 
