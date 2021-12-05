bfb124aa478ec55702ec9825e4dbf4e0abad6a56
Henri Casanova <henric@hawaii.edu>
2021-12-04 14:29:55 -1000

    Removed unneeded messages

--- include/wrench/services/compute/cloud/CloudComputeService.h
+++ include/wrench/services/compute/cloud/CloudComputeService.h
@@ -125,20 +125,11 @@ namespace wrench {
         /***********************/
         /** \cond INTERNAL    */
         /***********************/
-        void submitStandardJob(std::shared_ptr<StandardJob> job,
-                               const std::map<std::string, std::string> &service_specific_args);
 
         void submitCompoundJob(std::shared_ptr<CompoundJob> job,
                                const std::map<std::string, std::string> &service_specific_args) override {};
 
-        void submitPilotJob(std::shared_ptr<PilotJob> job,
-                            const std::map<std::string, std::string> &service_specific_args) override;
-
-//        void terminateStandardJob(std::shared_ptr<StandardJob> job) override;
-
         void terminateCompoundJob(std::shared_ptr<CompoundJob> job) override {};
-
-//        void terminatePilotJob(std::shared_ptr<PilotJob> job) override;
 
         void validateProperties();
 
