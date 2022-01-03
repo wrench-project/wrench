b0b5966ee57c0664699b0460c028835a55af9d99
Henri Casanova <henric@hawaii.edu>
2022-01-02 21:45:35 -1000

    CMake fixes

--- src/wrench/workflow/parallel_model/ParallelModel.cpp
+++ src/wrench/workflow/parallel_model/ParallelModel.cpp
@@ -50,7 +50,6 @@ namespace wrench {
      * @return a model instance
      **/
     std::shared_ptr<ParallelModel> ParallelModel::CUSTOM(std::function<std::vector<double>(double, long)> lambda) {
-        return std::shared_ptr<ParallelModel>(new CustomParallelModel(lambda));
     }
 
 };
