9b2af320710aeedd51bbc5a52c0b227cc5b3257d
Henri Casanova <henric@hawaii.edu>
2022-01-10 17:06:07 -1000

    Made wrench-init be built in build dir rather than in-source

--- conf/cmake/Tools.cmake
+++ conf/cmake/Tools.cmake
@@ -1,7 +1,7 @@
 add_custom_command(
         OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/tools/wrench/wrench-init/wrench-init
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/tools/wrench/wrench-init/
-        COMMAND /bin/sh generate.sh ${CMAKE_CURRENT_SOURCE_DIR}
+        COMMAND /bin/sh generate.sh ${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_CURRENT_BINARY_DIR}
         DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/tools/wrench/wrench-init/wrench-init.in
         DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/tools/wrench/wrench-init/generate.sh
         DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/tools/wrench/wrench-init/base_code/CMakeLists.txt
@@ -19,7 +19,7 @@ add_custom_command(
 add_custom_target(wrench_init ALL DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/tools/wrench/wrench-init/wrench-init)
 add_dependencies(wrench_init wrench)
 
-install(FILES ${CMAKE_CURRENT_SOURCE_DIR}/tools/wrench/wrench-init/wrench-init
+install(FILES ${CMAKE_CURRENT_BINARY_DIR}/tools/wrench/wrench-init/wrench-init
         DESTINATION bin
         PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE
         )
