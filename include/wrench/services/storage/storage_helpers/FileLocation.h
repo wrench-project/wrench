246ebfef6bedb543ff11be3d0fdc49d0df568cb1
Henri Casanova <henric@hawaii.edu>
2022-01-20 22:16:29 -1000

    debuging

--- include/wrench/services/storage/storage_helpers/FileLocation.h
+++ include/wrench/services/storage/storage_helpers/FileLocation.h
@@ -29,6 +29,10 @@ namespace wrench {
 
     public:
 
+        ~FileLocation() {
+            std::cerr << "IN FILE LOCATION DESTRUCTOR\n";
+        }
+
         /**
          * @brief Static location that denotes a compute service's scratch space
          */
