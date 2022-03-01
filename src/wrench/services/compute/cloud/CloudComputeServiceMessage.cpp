8ba92e824039bc0e0da916965aa1fa1e57eda086
Henri Casanova <henric@hawaii.edu>
2022-02-28 21:40:35 -1000

    Documentation fix

--- src/wrench/services/compute/cloud/CloudComputeServiceMessage.cpp
+++ src/wrench/services/compute/cloud/CloudComputeServiceMessage.cpp
@@ -16,7 +16,6 @@ namespace wrench {
     /**
      * @brief Constructor
      *
-     * @param name: the message name
      * @param payload: the message size in bytes
      */
     CloudComputeServiceMessage::CloudComputeServiceMessage(double payload) :
