3fb2c65fa285d60a8d1af245f8810f42c48aea56
Jesse McDonald <jamcdonald@lcmail.lcsc.edu>
2022-03-28 13:03:24 -1000

    started adding s4u

---
+++ include/wrench/services/storage/xrootd/XRootDMessagePayload.h
@@ -0,0 +1,49 @@
+//
+// Created by jamcdonald on 3/28/22.
+//
+
+#ifndef WRENCH_XROOTDMESSAGEPAYLOAD_H
+#define WRENCH_XROOTDMESSAGEPAYLOAD_H
+/**
+ * Copyright (c) 2017. The WRENCH Team.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+
+#include "wrench/services/ServiceMessagePayload.h"
+
+
+namespace wrench {
+    namespace XRootD{
+        /**
+         * @brief Configurable message payloads for a StorageService
+         */
+        class MessagePayload : public ServiceMessagePayload {
+
+        public:
+            /** @brief The number of bytes in the control message sent to the daemon to request its free space information **/
+
+            /** @brief The number of bytes in the control message sent to the daemon to request a file lookup **/
+            DECLARE_MESSAGEPAYLOAD_NAME(FILE_LOOKUP_REQUEST_MESSAGE_PAYLOAD);
+
+            /** @brief The number of bytes in the control message sent to the daemon to request a file deletion **/
+            DECLARE_MESSAGEPAYLOAD_NAME(FILE_DELETE_REQUEST_MESSAGE_PAYLOAD);
+
+            /** @brief The number of bytes in the control message sent to the daemon to request a file read **/
+            DECLARE_MESSAGEPAYLOAD_NAME(FILE_READ_REQUEST_MESSAGE_PAYLOAD);
+            /** @brief The number of bytes in the control message sent by the daemon to answer a file read request **/
+            DECLARE_MESSAGEPAYLOAD_NAME(FILE_READ_ANSWER_MESSAGE_PAYLOAD);
+            DECLARE_MESSAGEPAYLOAD_NAME(CONTINUE_SEARCH);
+            DECLARE_MESSAGEPAYLOAD_NAME(UPDATE_CACHE);
+
+        };
+    };
+};// namespace wrench
+
+
+
+#endif //WRENCH_XROOTDMESSAGEPAYLOAD_H
