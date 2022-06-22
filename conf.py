9884522c4912758811464500de9e0ea493fa7f00
Jesse McDonald <30709204+Jesse-McDonald@users.noreply.github.com>
2022-06-21 16:55:51 -1000

    extra file removed

--- conf.py
+++
@@ -1,8 +0,0 @@
-breathe_projects = {
-    "user": "../../docs/xrootd/user/xml/",
-    "developer": "../../docs/xrootd/developer/xml/",
-    "internal": "../../docs/xrootd/internal/xml/",
-}
-
-version = 'xrootd'
-release = 'xrootd'
