48e05f55ebc77a0cc725300c621994f3fea39e5e
Henri Casanova <henric@hawaii.edu>
2022-08-31 17:48:30 -1000

    Some code changes / cleanup / formatting

--- examples/action_api/XRootD/Simulator.cpp
+++ examples/action_api/XRootD/Simulator.cpp
@@ -11,6 +11,9 @@
 /**
  ** This is the main function for a WRENCH simulator. The simulator takes
  ** as input an XML platform description file.
+ **
+ ** Example invocation of the simulator with no logging:
+ **    ./wrench-example-xrootd-basic ./xrootd_platform.xml
  **/
 
 #include <iostream>
@@ -35,71 +38,77 @@ int main(int argc, char **argv) {
 
     /* Initialize the simulation */
     simulation->init(&argc, argv);
-	bool reduced=false;
+    bool reduced=false;
+
     /* Parsing of the command-line arguments */
-    if (argc 2|| argc>3) {
+    if (argc  != 2) {
         std::cerr << "Usage: " << argv[0] << " <xml platform file>  [--log=controller.threshold=info | --wrench-full-log]" << std::endl;
         exit(1);
     }
-	if(argc==3){
-		reduced=strcmp(argv[2],"true");
-	}
+
     /* Instantiating the simulated platform */
     simulation->instantiatePlatform(argv[1]);
 
-    /* Instantiate a storage service on the platform */
-    //auto storage_service = simulation->add(new wrench::SimpleStorageService("StorageHost", {"/"}, {}, {}));
-
     /* Instantiate a bare-metal compute service on the platform */
     auto baremetal_service = simulation->add(new wrench::BareMetalComputeService("user", {"user"}, "", {}, {}));
-	simulation->add(baremetal_service);
+    simulation->add(baremetal_service);
+
+    /* Create an XRootD manager object, with a couple of configuration properties.
+     * The REDUCED_SIMULATION property, when set to "true" ("false" is the default),
+     * abstracts away more simulation details, which speeds up the simulation but
+     * may make it less realistic. See the documentation for more details.
+     */
+    wrench::XRootD::XRootD xrootdManager(simulation,
+                                         {
+                                                 {wrench::XRootD::Property::CACHE_MAX_LIFETIME,"28800"},
+                                                 {wrench::XRootD::Property::REDUCED_SIMULATION,"false"}
+                                         },
+                                         {});
+
+    /* Construct an XRootD tree as follows (vertices are host names)
+
+            root
+         /   |   \
+    leaf1  leaf2  super1
+                 /   |   \
+            leaf3  leaf4  super2
+                         /   |   \
+                    leaf5  leaf6  super3
+                                 /   |   \
+                            leaf7  leaf8  super4
+                                         /   |   \
+                                    leaf9  leaf10  leaf11
+    */
+    std::shared_ptr<wrench::XRootD::Node> root=xrootdManager.createSupervisor("root");
+    root->addChild(xrootdManager.createStorageServer("leaf1","/",{},{}));
+    root->addChild(xrootdManager.createStorageServer("leaf2","/",{},{}));
+    auto super1 = xrootdManager.createSupervisor("super1");
+    root->addChild(super1);
+
+    super1->addChild(xrootdManager.createStorageServer("leaf3","/",{},{}));
+    super1->addChild(xrootdManager.createStorageServer("leaf4","/",{},{}));
+    auto super2 = xrootdManager.createSupervisor("super2");
+    super1->addChild(super2);
+
+    super2->addChild(xrootdManager.createStorageServer("leaf5","/",{},{}));
+    super2->addChild(xrootdManager.createStorageServer("leaf6","/",{},{}));
+    auto super3 = xrootdManager.createSupervisor("super3");
+    super2->addChild(super3);
+
+    super3->addChild(xrootdManager.createStorageServer("leaf7","/",{},{}));
+    super3->addChild(xrootdManager.createStorageServer("leaf8","/",{},{}));
+    auto super4 = xrootdManager.createSupervisor("super4");
+    super3->addChild(super4);
+    super4->addChild(xrootdManager.createStorageServer("leaf9","/",{},{}));
+    super4->addChild(xrootdManager.createStorageServer("leaf10","/",{},{}));
+    super4->addChild(xrootdManager.createStorageServer("leaf11","/",{},{}));
 
     /* Instantiate an execution controller */
-    
-	/*Construct XRootD tree that looks like
-		    Root
-	     /   |   \ 	
-	Leaf1  Leaf2  Super1			
-				 /   |   \ 	
-			Leaf3  Leaf4  Super2
-						 /   |   \ 	
-					Leaf5  Leaf6  Super3
-								 /   |   \ 	
-							Leaf7  Leaf8  Super4
-										 /   |   \ 	
-									Leaf9  Leaf10  Leaf11
-	        
-	*/
-	wrench::XRootD::XRootD xrootdManager(simulation,{{wrench::XRootD::Property::CACHE_MAX_LIFETIME,"28800"},
-                                                  {wrench::XRootD::Property::REDUCED_SIMULATION,(reduced ? "true" : "false")}},{});
-	std::shared_ptr<wrench::XRootD::Node> root=xrootdManager.createSupervisor("root");
-	root->addChild(xrootdManager.createStorageServer("leaf1","/",{},{}));
-	root->addChild(xrootdManager.createStorageServer("leaf2","/",{},{}));
-	std::shared_ptr<wrench::XRootD::Node> activeNode=xrootdManager.createSupervisor("super1");
-	root->addChild(activeNode);
-	activeNode->addChild(xrootdManager.createStorageServer("leaf3","/",{},{}));
-	activeNode->addChild(xrootdManager.createStorageServer("leaf4","/",{},{}));
-	std::shared_ptr<wrench::XRootD::Node> previousNode=activeNode;
-	activeNode=xrootdManager.createSupervisor("super2");
-	previousNode->addChild(activeNode);
-	activeNode->addChild(xrootdManager.createStorageServer("leaf5","/",{},{}));
-	activeNode->addChild(xrootdManager.createStorageServer("leaf6","/",{},{}));
-	previousNode=activeNode;
-	activeNode=xrootdManager.createSupervisor("super3");
-	previousNode->addChild(activeNode);
-	activeNode->addChild(xrootdManager.createStorageServer("leaf7","/",{},{}));
-	activeNode->addChild(xrootdManager.createStorageServer("leaf8","/",{},{}));
-	previousNode=activeNode;
-	activeNode=xrootdManager.createSupervisor("super4");
-	previousNode->addChild(activeNode);
-	activeNode->addChild(xrootdManager.createStorageServer("leaf9","/",{},{}));
-	activeNode->addChild(xrootdManager.createStorageServer("leaf10","/",{},{}));
-	activeNode->addChild(xrootdManager.createStorageServer("leaf11","/",{},{}));
-	
-	/* Launch the simulation */
-	auto controller = simulation->add(        new wrench::Controller(baremetal_service, root,&xrootdManager, "root"));
+    auto controller = simulation->add(new wrench::Controller(baremetal_service, root,&xrootdManager, "root"));
+
+    /* Launch the simulation */
     simulation->launch();
 
     return 0;
-	
-	}
+
+}
