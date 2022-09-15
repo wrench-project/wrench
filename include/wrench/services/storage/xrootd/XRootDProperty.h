7f958e196a432544072f54453f4ce899da89ee62
Jesse McDonald <jamcdonald@lcmail.lcsc.edu>
2022-09-14 16:58:46 -1000

    added locate unit test

--- include/wrench/services/storage/xrootd/XRootDProperty.h
+++ include/wrench/services/storage/xrootd/XRootDProperty.h
@@ -21,7 +21,7 @@ namespace wrench {
             DECLARE_PROPERTY_NAME(SEARCH_BROADCAST_OVERHEAD);
             /** @brief The overhead to handle a cache update, in flops **/
             DECLARE_PROPERTY_NAME(UPDATE_CACHE_OVERHEAD);
-            /** @brief The time an entry will remain in the cache before being erased, in seconds **/
+            /** @brief The time an entry will remain in the cache before being erased, in Default: "infinity", Default unit: second. Example: "30", "20s", "100ms", etc. **/
             DECLARE_PROPERTY_NAME(CACHE_MAX_LIFETIME);
             /** @brief If set to "true", then the simulation of the XRootD search does not simulate all
              * control message sends/receives, but just those to the node that the search will find (which
