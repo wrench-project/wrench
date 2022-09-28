90aa34262d09815b1508c89161b5c0893fbde1e7
Henri Casanova <henric@hawaii.edu>
2022-09-27 18:21:13 -1000

    Remove all useless TTLs Example touch-ups coverage++

--- test/services/compute_services/bare_metal_compound_jobs/BareMetalComputeServiceMultiActionTests.cpp
+++ test/services/compute_services/bare_metal_compound_jobs/BareMetalComputeServiceMultiActionTests.cpp
@@ -716,7 +716,7 @@ private:
 
         auto sleep1 = job->addSleepAction("sleep1", 10.0);
         auto file_read = job->addFileReadAction("file_read", this->test->input_file,
-                                                wrench::FileLocation::LOCATION(this->test->storage_service1),
+                                                this->test->storage_service1,
                                                 this->test->input_file->getSize());
         auto sleep_after_file_read = job->addSleepAction("sleep_after_file_read", 10.0);
         auto compute = job->addComputeAction("compute", 10000.0, 100.0, 1, 1, wrench::ParallelModel::AMDAHL(1.0));
