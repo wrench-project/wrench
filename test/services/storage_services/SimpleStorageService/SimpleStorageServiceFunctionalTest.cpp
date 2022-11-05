3c4e5ba65fdff6c5ada28101e3df8efbe9c35ccc
Henri Casanova <henric@hawaii.edu>
2022-11-04 15:20:36 -1000

    bug--

--- test/services/storage_services/SimpleStorageService/SimpleStorageServiceFunctionalTest.cpp
+++ test/services/storage_services/SimpleStorageService/SimpleStorageServiceFunctionalTest.cpp
@@ -594,7 +594,7 @@ void SimpleStorageServiceFunctionalTest::do_BasicFunctionality_test() {
 
     // Create and initialize a simulation
     auto simulation = wrench::Simulation::createSimulation();
-    int argc = 2;
+    int argc = 3;
     char **argv = (char **) calloc(argc, sizeof(char *));
     argv[0] = strdup("unit_test");
     argv[1] = strdup("--wrench-full-log");
