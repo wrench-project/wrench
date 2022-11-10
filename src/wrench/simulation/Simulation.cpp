3fe4d280e8f23186774dfe00303204d257ee766b
Henri Casanova <henric@hawaii.edu>
2022-11-09 17:21:15 -1000

    test insanity fixed

--- src/wrench/simulation/Simulation.cpp
+++ src/wrench/simulation/Simulation.cpp
@@ -1570,7 +1570,6 @@ namespace wrench {
      * @param file : file to remove
      */
     void Simulation::removeFile(const std::shared_ptr<DataFile> &file) {
-        std::cerr << "IN REMOVE FILE FOR FILE (total number= " << Simulation::getFileMap().size() << "): " << file->getID() << "\n";
         if (Simulation::data_files.find(file->getID()) == Simulation::data_files.end()) {
             throw std::invalid_argument("Simulation::removeFile(): Unknown file");
         }
