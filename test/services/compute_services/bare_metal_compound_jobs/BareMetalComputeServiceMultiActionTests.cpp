33bd8a88da6d838f97befab8a793fa02704cb79e
Henri Casanova <henric@hawaii.edu>
2022-12-13 09:13:10 -1000

    Coverage++ testing++

--- test/services/compute_services/bare_metal_compound_jobs/BareMetalComputeServiceMultiActionTests.cpp
+++ test/services/compute_services/bare_metal_compound_jobs/BareMetalComputeServiceMultiActionTests.cpp
@@ -629,6 +629,8 @@ private:
         if (not std::dynamic_pointer_cast<wrench::SomeActionsHaveFailed>(real_event->failure_cause)) {
             throw std::runtime_error("Unexpected job-level failure cause");
         }
+        auto real_cause = std::dynamic_pointer_cast<wrench::SomeActionsHaveFailed>(real_event->failure_cause);
+        real_cause->toString(); // coverage
 
         // Check job state
         if (job->getState() != wrench::CompoundJob::State::DISCONTINUED) {
