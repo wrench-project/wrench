e369d8be184dfad579f1e7a13412a5edaa8f8e2e
Jesse McDonald <jamcdonald@lcmail.lcsc.edu>
2023-01-26 17:16:01 -1000

    cache fixes

--- include/wrench/services/storage/proxy/StorageServiceProxy.h
+++ include/wrench/services/storage/proxy/StorageServiceProxy.h
@@ -28,8 +28,8 @@ namespace wrench {
         using StorageService::writeFile;
         using StorageService::readFile;
 
-        int main();
-        bool processNextMessage();
+
+
         std::map<std::string, double> getTotalSpace();
         std::map<std::string, double> getFreeSpace();
 
@@ -44,10 +44,6 @@ namespace wrench {
 	 * @return a (simulated) date in seconds
 	 */
         double getFileLastWriteDate(const std::shared_ptr<FileLocation> &location);//forward
-
-
-
-
 
         virtual void deleteFile(const std::shared_ptr<StorageService>& targetServer, const std::shared_ptr<DataFile> &file, const std::shared_ptr<FileRegistryService> &file_registry_service = nullptr);
 
@@ -70,14 +66,8 @@ namespace wrench {
 
 
         double getLoad() ;//cache
-
-        /***********************/
-        /** \cond INTERNAL    **/
-
-
-        StorageServiceProxy(const std::string &hostname, const std::shared_ptr<StorageService>& cache=nullptr,const std::shared_ptr<StorageService>& defaultRemote=nullptr,WRENCH_PROPERTY_COLLECTION_TYPE properties={},WRENCH_MESSAGE_PAYLOADCOLLECTION_TYPE messagePayload={});
-        /**
-         * @brief Factory to create a StorageServiceProxy that does not have a default destination to forward requests too, but will cache requests as they are made
+                          /**
+         * @brief Factory to create a StorageServiceProxy that can a default destination to forward requests too, and will cache requests as they are made
          * @param hostname: hostname
          * @param cache: The StorageService to use as a Cache
          * @param remote: The StorageService to use as a remote file source
@@ -91,6 +81,13 @@ namespace wrench {
                 const std::shared_ptr<StorageService> &remote = nullptr, WRENCH_PROPERTY_COLLECTION_TYPE properties = {}, WRENCH_MESSAGE_PAYLOADCOLLECTION_TYPE messagePayload = {}) {
             return std::make_shared<StorageServiceProxy>(hostname,cache,remote,properties,messagePayload);
         }
+        /***********************/
+        /** \cond INTERNAL    **/
+        /***********************/
+        int main();
+        bool processNextMessage();
+        StorageServiceProxy(const std::string &hostname, const std::shared_ptr<StorageService>& cache=nullptr,const std::shared_ptr<StorageService>& defaultRemote=nullptr,WRENCH_PROPERTY_COLLECTION_TYPE properties={},WRENCH_MESSAGE_PAYLOADCOLLECTION_TYPE messagePayload={});
+
         /**
          * @brief Factory to create a StorageServiceProxy that does not cache reads, and does not have a default destination to forward too
          * @param hostname: hostname
@@ -152,34 +149,52 @@ namespace wrench {
 
         };
     };
+    /***********************/
+    /** \endcond          **/
+    /***********************/
 
     class ProxyLocation : public FileLocation{
         public:
             const std::shared_ptr<StorageService> target;
-            static std::shared_ptr<ProxyLocation> LOCATION(const std::shared_ptr<StorageService>& target, const std::shared_ptr<FileLocation>& other){
-                return std::shared_ptr<ProxyLocation>(new ProxyLocation(target,other));
+            static std::shared_ptr<ProxyLocation> LOCATION(
+                    const std::shared_ptr<StorageService> &target,
+                    const std::shared_ptr<FileLocation> &other) {
+                return std::shared_ptr<ProxyLocation>(new ProxyLocation(target, other));
             }
-            static std::shared_ptr<ProxyLocation> LOCATION(const std::shared_ptr<StorageService>& target,const std::shared_ptr<StorageService> &ss, const std::shared_ptr<DataFile> &file){
-                return std::shared_ptr<ProxyLocation>(new ProxyLocation(target,FileLocation::LOCATION(ss,file)));
+            static std::shared_ptr<ProxyLocation> LOCATION(
+                    const std::shared_ptr<StorageService> &target,
+                    const std::shared_ptr<StorageService> &ss,
+                    const std::shared_ptr<DataFile> &file) {
+                return std::shared_ptr<ProxyLocation>(new ProxyLocation(target, FileLocation::LOCATION(ss, file)));
             }
 
-            static std::shared_ptr<ProxyLocation> LOCATION(const std::shared_ptr<StorageService>& target,const std::shared_ptr<StorageService> &ss,
-                                                          std::shared_ptr<StorageService> server_ss,
-                                                          const std::shared_ptr<DataFile> &file){
-                return std::shared_ptr<ProxyLocation>(new ProxyLocation(target,FileLocation::LOCATION(ss,server_ss,file)));
+            static std::shared_ptr<ProxyLocation> LOCATION(
+                    const std::shared_ptr<StorageService> &target,
+                    const std::shared_ptr<StorageService> &ss,
+                   std::shared_ptr<StorageService> server_ss,
+                   const std::shared_ptr<DataFile> &file) {
+                return std::shared_ptr<ProxyLocation>(new ProxyLocation(target, FileLocation::LOCATION(ss, server_ss, file)));
             }
-            static std::shared_ptr<ProxyLocation> LOCATION(const std::shared_ptr<StorageService>& target,const std::shared_ptr<StorageService> &ss,
-                                                          std::string absolute_path,
-                                                          const std::shared_ptr<DataFile> &file){
-                return std::shared_ptr<ProxyLocation>(new ProxyLocation(target,FileLocation::LOCATION(ss,absolute_path,file)));
+        static std::shared_ptr<ProxyLocation> LOCATION(
+                    const std::shared_ptr<StorageService> &target,
+                    const std::shared_ptr<StorageService> &ss,
+                    std::string absolute_path,
+                    const std::shared_ptr<DataFile> &file) {
+                return std::shared_ptr<ProxyLocation>(new ProxyLocation(target, FileLocation::LOCATION(ss, ss->getMountPoint()+"/"+absolute_path, file)));
             }
         private:
+            /***********************/
+            /** \cond INTERNAL    **/
+            /***********************/
             //TODO unique instance factory
             ProxyLocation(const std::shared_ptr<StorageService>& target, const std::shared_ptr<FileLocation>& other):FileLocation(*other),target(target){
                 if (target == nullptr) {
                     throw std::invalid_argument("ProxyLocation::LOCATION(): Cannot pass nullptr target");
                 }
             }
+            /***********************/
+            /** \endcond           */
+            /***********************/
     };
     /***********************/
     /** \endcond           */
