e21dd6b318e778ddf078cc5b62fa2648c10fcbaa
Henri Casanova <henric@hawaii.edu>
2023-02-21 14:37:31 -1000

    comments++

--- include/wrench/services/storage/StorageService.h
+++ include/wrench/services/storage/StorageService.h
@@ -34,87 +34,185 @@ namespace wrench {
         /** \cond DEVELOPER   **/
         /***********************/
 
+        /**
+         * @brief Stop the servier
+         */
         void stop() override;
 
         /** File Lookup methods (in simulation) **/
+
+        /**
+         * @brief Lookup whether a file exists at a location (incurs simulated overheads)
+         * @param location a location
+         * @return true if the file is present at the location, or false
+         */
         static bool lookupFileAtLocation(const std::shared_ptr<FileLocation> &location) {
             if (location == nullptr) {
                 throw std::invalid_argument("StorageService::lookupFileAtLocation(): invalid argument argument");
             }
             return location->getStorageService()->lookupFile(location);
         }
+        /**
+         * @brief Lookup whether a file exists on the storage service (incurs simulated overheads)
+         * @param file a file
+         * @return true if the file is present, or false
+         */
         bool lookupFile(const std::shared_ptr<DataFile> &file) {
             return this->lookupFile(file, "/");
         }
+        /**
+         * @brief Lookup whether a file exists on the storage service (incurs simulated overheads)
+         * @param file a file
+         * @param path a path
+         * @return true if the file is present, or false
+         */
         virtual bool lookupFile(const std::shared_ptr<DataFile> &file, const std::string &path) {
             return this->lookupFile(wrench::FileLocation::LOCATION(this->getSharedPtr<StorageService>(), path, file));
         }
+        /**
+         * @brief Lookup whether a file exists at a location on the storage service (incurs simulated overheads)
+         * @param location a location
+         * @return true if the file is present, or false
+         */
         virtual bool lookupFile(const std::shared_ptr<FileLocation> &location) {
             return this->lookupFile(S4U_Daemon::getRunningActorRecvMailbox(), location);
         }
 
         /** File deletion methods **/
+
+        /**
+         * @brief Delete a file at a location (incurs simulated overheads)
+         * @param location a location
+         */
         static void deleteFileAtLocation(const std::shared_ptr<FileLocation> &location) {
             if (location == nullptr) {
                 throw std::invalid_argument("StorageService::deleteFileAtLocation(): invalid argument argument");
             }
             location->getStorageService()->deleteFile(location);
         }
+        /**
+         * @brief Delete a file at the storage service (incurs simulated overheads)
+         * @param file a file
+         */
         void deleteFile(const std::shared_ptr<DataFile> &file) {
             this->deleteFile(file, "/");
         }
+        /**
+         * @brief Delete a file at the storage service (incurs simulated overheads)
+         * @param file a file
+         * @param path a path
+         */
         virtual void deleteFile(const std::shared_ptr<DataFile> &file, const std::string &path) {
             this->deleteFile(wrench::FileLocation::LOCATION(this->getSharedPtr<StorageService>(), path, file));
         }
+        /**
+         * @brief Delete a file at the storage service (incurs simulated overheads)
+         * @param file a location
+         */
         virtual void deleteFile(const std::shared_ptr<FileLocation> &location) {
             this->deleteFile(S4U_Daemon::getRunningActorRecvMailbox(), location, true);
         }
 
         /** File read methods **/
+        /**
+         * @brief Read a file at a location (incurs simulated overheads)
+         * @param location a location
+         */
         static void readFileAtLocation(const std::shared_ptr<FileLocation> &location) {
             if (location == nullptr) {
                 throw std::invalid_argument("StorageService::readFileAtLocation(): invalid argument argument");
             }
             location->getStorageService()->readFile(location, location->getFile()->getSize());
         }
+        /**
+         * @brief Read a file at a location (incurs simulated overheads)
+         * @param location a location
+         * @param num_bytes a number of bytes to read
+         */
         static void readFileAtLocation(const std::shared_ptr<FileLocation> &location, double num_bytes) {
             if (location == nullptr) {
                 throw std::invalid_argument("StorageService::readFileAtLocation(): invalid argument argument");
             }
             location->getStorageService()->readFile(location, num_bytes);
         }
+        /**
+         * @brief Read a file at the storage service (incurs simulated overheads)
+         * @param file a file
+         */
         void readFile(const std::shared_ptr<DataFile> &file) {
             this->readFile(file, "/", file->getSize());
         }
+        /**
+         * @brief Read a file at the storage service (incurs simulated overheads)
+         * @param file a file
+         * @param num_bytes a number of bytes to read
+         */
         void readFile(const std::shared_ptr<DataFile> &file, double num_bytes) {
             this->readFile(file, "/", num_bytes);
         }
+        /**
+         * @brief Read a file at the storage service (incurs simulated overheads)
+         * @param file a file
+         * @param path a path
+         */
         virtual void readFile(const std::shared_ptr<DataFile> &file, const std::string &path) {
             this->readFile(file, path, file->getSize());
         }
+        /**
+         * @brief Read a file at the storage service (incurs simulated overheads)
+         * @param file a file
+         * @param path a path
+         * @param num_bytes a number of bytes to read
+         */
         virtual void readFile(const std::shared_ptr<DataFile> &file, const std::string &path, double num_bytes) {
             this->readFile(wrench::FileLocation::LOCATION(this->getSharedPtr<StorageService>(), path, file), num_bytes);
         }
+        /**
+         * @brief Read a file at the storage service (incurs simulated overheads)
+         * @param location a location
+         */
         void readFile(const std::shared_ptr<FileLocation> &location) {
             this->readFile(S4U_Daemon::getRunningActorRecvMailbox(), location, location->getFile()->getSize(), true);
         }
+        /**
+         * @brief Read a file at the storage service (incurs simulated overheads)
+         * @param location a location
+         * @param num_bytes a number of bytes to read
+         */
         virtual void readFile(const std::shared_ptr<FileLocation> &location, double num_bytes) {
             this->readFile(S4U_Daemon::getRunningActorRecvMailbox(), location, num_bytes, true);
         }
 
         /** File write methods **/
+        /**
+         * @brief Write a file at a location (incurs simulated overheads)
+         * @param location a location
+         */
         static void writeFileAtLocation(const std::shared_ptr<FileLocation> &location) {
             if (location == nullptr) {
                 throw std::invalid_argument("StorageService::writeFileAtLocation(): invalid argument argument");
             }
             location->getStorageService()->writeFile(location);
         }
+        /**
+         * @brief Write a file at the storage service (incurs simulated overheads)
+         * @param file a file
+         */
         void writeFile(const std::shared_ptr<DataFile> &file) {
             this->writeFile(file, "/");
         }
+        /**
+         * @brief Write a file at the storage service (incurs simulated overheads)
+         * @param file a file
+         * @param path a path
+         */
         virtual void writeFile(const std::shared_ptr<DataFile> &file, const std::string &path) {
             this->writeFile(wrench::FileLocation::LOCATION(this->getSharedPtr<StorageService>(), path, file));
         }
+        /**
+         * @brief Write a file at the storage service (incurs simulated overheads)
+         * @param location a location
+         */
         virtual void writeFile(const std::shared_ptr<FileLocation> &location) {
             this->writeFile(S4U_Daemon::getRunningActorRecvMailbox(), location, true);
         }
