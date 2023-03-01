9d9a2fde992c1382288b6920f1417dae42452712
Henri Casanova <henric@hawaii.edu>
2023-02-28 20:50:24 -1000

    Added a communicator example

--- src/wrench/communicator/Communicator.cpp
+++ src/wrench/communicator/Communicator.cpp
@@ -42,26 +42,28 @@ namespace wrench {
      * @return the desired rank
      */
     unsigned long Communicator::join(unsigned long desired_rank) {
+        auto my_pid = simgrid::s4u::this_actor::get_pid();
+
         if (desired_rank >= this->size) {
             throw std::invalid_argument("Communicator::join(): invalid arguments");
 
         }
         if (this->rank_to_mailbox.find(desired_rank) != this->rank_to_mailbox.end()) {
-            if (this->actor_to_rank.find(simgrid::s4u::this_actor::get_pid()) != this->actor_to_rank.end()) {
-                return this->actor_to_rank[simgrid::s4u::this_actor::get_pid()];
+            if (this->actor_to_rank.find(my_pid) != this->actor_to_rank.end()) {
+                return this->actor_to_rank[my_pid];
             } else {
                 throw std::invalid_argument("Communicator::join(): rank already used by another participant");
             }
         }
 
         this->rank_to_mailbox[desired_rank] = S4U_Mailbox::getTemporaryMailbox();
-        this->actor_to_rank[simgrid::s4u::this_actor::get_pid()] = desired_rank;
+        this->actor_to_rank[my_pid] = desired_rank;
 
         if (this->size > this->rank_to_mailbox.size()) {
             simgrid::s4u::this_actor::suspend();
         } else {
             for (auto const &item : this->actor_to_rank) {
-                if (item.first != simgrid::s4u::this_actor::get_pid()) {
+                if (item.first != my_pid) {
                     simgrid::s4u::Actor::by_pid(item.first)->resume();
                 }
             }
@@ -70,26 +72,74 @@ namespace wrench {
         return desired_rank;
     }
 
-
     /**
-     * @brief Perform a communication step
+     * @brief Perform asynchronous sends and receives
      * @param sends: the specification of all outgoing communications as <rank, volume in bytes> pairs
      * @param num_receives: the number of expected received (from any source)
      */
-    void Communicator::communicate(const std::map<unsigned long, double>& sends, int num_receives) {
+    void Communicator::sendAndReceive(const std::map<unsigned long, double> &sends, int num_receives) {
+        this->sendReceiveAndCompute(sends, num_receives, 0);
+    }
+
+
+    /**
+     * @brief Perform concurrent asynchronous sends, receives, and a computation
+     * @param sends: the specification of all outgoing communications as <rank, volume in bytes> pairs
+     * @param num_receives: the number of expected received (from any source)
+     * @param flops: the number of floating point operations to compute
+     */
+    void Communicator::sendReceiveAndCompute(const std::map<unsigned long, double> &sends, int num_receives, double flops) {
+        auto my_pid = simgrid::s4u::this_actor::get_pid();
+
+        if (this->actor_to_rank.find(my_pid) == this->actor_to_rank.end()) {
+            throw std::invalid_argument("Communicator::communicate(): Calling process is not part of this communicator");
+        }
         // Post all the sends
         std::vector<std::shared_ptr<S4U_PendingCommunication>> posted_sends;
         for (auto const &send_operation : sends) {
             auto dst_mailbox = this->rank_to_mailbox[send_operation.first];
             posted_sends.push_back(S4U_Mailbox::iputMessage(dst_mailbox, new wrench::SimulationMessage(send_operation.second)));
         }
+        // Post the computation (if any)
+        simgrid::s4u::ExecPtr computation = nullptr;
+        if (flops > 0) {
+            computation = simgrid::s4u::this_actor::exec_init(flops);
+        }
+
         // Do all the synchronous receives
         for (int i=0; i < num_receives; i++) {
-            S4U_Mailbox::getMessage(this->rank_to_mailbox[this->actor_to_rank[simgrid::s4u::this_actor::get_pid()]]);
+            S4U_Mailbox::getMessage(this->rank_to_mailbox[this->actor_to_rank[my_pid]]);
         }
         // Wait for all the sends
         for (auto const &posted_send : posted_sends) {
             posted_send->wait();
+        }
+        // Wait for the computation
+        if (computation) {
+            computation->wait();
+        }
+    }
+
+    /**
+     * @brief Barrier method (all participants wait for each other)
+     */
+    void Communicator::barrier() {
+        auto my_pid = simgrid::s4u::this_actor::get_pid();
+        static unsigned long count = 0;
+        if (this->actor_to_rank.find(my_pid) == this->actor_to_rank.end()) {
+            throw std::invalid_argument("Communicator::barrier(): Calling process is not part of this communicator");
+        }
+
+        count++;
+        if (count < this->size) {
+            simgrid::s4u::this_actor::suspend();
+        } else {
+            count = 0;
+            for (auto const &item : this->actor_to_rank) {
+                if (item.first != my_pid) {
+                    simgrid::s4u::Actor::by_pid(item.first)->resume();
+                }
+            }
         }
     }
 
@@ -101,5 +151,6 @@ namespace wrench {
             S4U_Mailbox::retireTemporaryMailbox(item.second);
         }
     }
+
 
 }
