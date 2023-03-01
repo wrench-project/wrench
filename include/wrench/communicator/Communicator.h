d741c12ef05554524ef9fd1bddf01eb374037857
Henri Casanova <henric@hawaii.edu>
2023-02-28 18:32:14 -1000

    Implemented a communicator feature Added a communicator test

---
+++ include/wrench/communicator/Communicator.h
@@ -0,0 +1,38 @@
+
+
+#ifndef WRENCH_COMMUNICATOR_H
+#define WRENCH_COMMUNICATOR_H
+
+#include <memory>
+#include <map>
+#include "wrench/simgrid_S4U_util/S4U_Daemon.h"
+
+namespace wrench {
+
+    class Communicator {
+
+    public:
+        static std::shared_ptr<Communicator> createCommunicator(unsigned long size);
+
+        unsigned long join();
+        unsigned long join(unsigned long desired_rank);
+        unsigned long getNumRanks();
+        void communicate(const std::map<unsigned long, double>& sends, int num_receives);
+
+        ~Communicator();
+
+
+    protected:
+        explicit Communicator(unsigned long size) : size(size) {};
+
+    private:
+        unsigned long size;
+        std::map<aid_t, unsigned long> actor_to_rank;
+        std::map<unsigned long, simgrid::s4u::Mailbox *> rank_to_mailbox;
+
+    };
+
+}
+
+
+#endif//WRENCH_COMMUNICATOR_H
