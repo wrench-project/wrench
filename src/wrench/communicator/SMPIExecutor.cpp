1803f8a9dc5c432dc5deee7bf33735082464361c
Henri Casanova <henric@hawaii.edu>
2023-03-03 14:21:14 -1000

    Using a barrier instead of messages to synchronize SMPI executor processes

--- src/wrench/communicator/SMPIExecutor.cpp
+++ src/wrench/communicator/SMPIExecutor.cpp
@@ -1,11 +1,10 @@
 
 #include <algorithm>
+#include <utility>
 
 #include <wrench/logging/TerminalOutput.h>
 #include "wrench/communicator/SMPIExecutor.h"
 #include "wrench/simulation/Simulation.h"
-#include "wrench/simgrid_S4U_util/S4U_Mailbox.h"
-#include "wrench/simgrid_S4U_util/S4U_PendingCommunication.h"
 #include "smpi/smpi.h"
 #include "simgrid/s4u.hpp"
 
@@ -25,7 +24,7 @@ namespace wrench {
          * @param data_size: number of data_size to send/recv
          * @param notify_mailbox: mailbox to notify when done
          */
-        MPI_Alltoall_partipant(int data_size, simgrid::s4u::Mailbox *notify_mailbox) : data_size(data_size), notify_mailbox(notify_mailbox) {}
+        MPI_Alltoall_partipant(int data_size, simgrid::s4u::BarrierPtr barrier) : data_size(data_size), barrier(std::move(barrier)) {}
 
         void operator ()() {
 
@@ -35,13 +34,12 @@ namespace wrench {
             SMPI_SHARED_FREE(data);
             MPI_Finalize();
 
-            // Notify my creator of completion
-            S4U_Mailbox::putMessage(notify_mailbox, new SimulationMessage(0));
+            barrier->wait();
         }
 
     private:
         int data_size;
-        simgrid::s4u::Mailbox *notify_mailbox;
+        simgrid::s4u::BarrierPtr barrier;
     };
 
 
@@ -52,16 +50,12 @@ namespace wrench {
      */
     void SMPIExecutor::performAlltoall(std::vector<simgrid::s4u::Host *> &hosts,
                                        int data_size) {
-        // Create a mailbox to receive notifications of completion
-        auto mailbox = S4U_Mailbox::getTemporaryMailbox();
+        auto barrier = simgrid::s4u::Barrier::create(hosts.size());
         // Start actors to do an MPI_AllToAll
         SMPI_app_instance_start(("MPI_AllToAll_" + std::to_string(simgrid::s4u::this_actor::get_pid())).c_str(),
-                                MPI_Alltoall_partipant(data_size, mailbox),
+                                MPI_Alltoall_partipant(data_size, barrier),
                                 hosts);
-        // Wait for all of those actors to be done
-        for (int i=0; i < hosts.size(); i++) {
-            S4U_Mailbox::getMessage(mailbox);
-        }
+        barrier->wait();
     }
 
 
@@ -74,7 +68,7 @@ namespace wrench {
          * @brief Constructor
          * @param notify_mailbox: mailbox to notify when done
          */
-        explicit MPI_Barrier_partipant(simgrid::s4u::Mailbox *notify_mailbox) : notify_mailbox(notify_mailbox) {}
+        explicit MPI_Barrier_partipant(simgrid::s4u::BarrierPtr barrier) : barrier(std::move(barrier)) {}
 
         void operator ()() {
 
@@ -82,12 +76,11 @@ namespace wrench {
             MPI_Barrier(MPI_COMM_WORLD);
             MPI_Finalize();
 
-            // Notify my creator of completion
-            S4U_Mailbox::putMessage(notify_mailbox, new SimulationMessage(0));
+            barrier->wait();
         }
 
     private:
-        simgrid::s4u::Mailbox *notify_mailbox;
+        simgrid::s4u::BarrierPtr  barrier;
     };
 
 
@@ -96,16 +89,12 @@ namespace wrench {
      * @param hosts: list of hosts
      */
     void SMPIExecutor::performBarrier(std::vector<simgrid::s4u::Host *> &hosts) {
-        // Create a mailbox to receive notifications of completion
-        auto mailbox = S4U_Mailbox::getTemporaryMailbox();
+        auto barrier = simgrid::s4u::Barrier::create(hosts.size() + 1);
         // Start actors to do an MPI_AllToAll
         SMPI_app_instance_start(("MPI_Barrier_" + std::to_string(simgrid::s4u::this_actor::get_pid())).c_str(),
-                                MPI_Barrier_partipant(mailbox),
+                                MPI_Barrier_partipant(barrier),
                                 hosts);
-        // Wait for all of those actors to be done
-        for (int i=0; i < hosts.size(); i++) {
-            S4U_Mailbox::getMessage(mailbox);
-        }
+        barrier->wait();
     }
 
 
@@ -119,7 +108,7 @@ namespace wrench {
          * @param data_size: number of data_size to send/recv
          * @param notify_mailbox: mailbox to notify when done
          */
-        MPI_Bcast_partipant(int data_size, simgrid::s4u::Mailbox *notify_mailbox) : data_size(data_size), notify_mailbox(notify_mailbox) {}
+        MPI_Bcast_partipant(int data_size, simgrid::s4u::BarrierPtr barrier) : data_size(data_size), barrier(std::move(barrier)) {}
 
         void operator ()() {
 
@@ -129,13 +118,13 @@ namespace wrench {
             SMPI_SHARED_FREE(data);
             MPI_Finalize();
 
-            // Notify my creator of completion
-            S4U_Mailbox::putMessage(notify_mailbox, new SimulationMessage(0));
+            // synchronize with my creator
+            barrier->wait();
         }
 
     private:
         int data_size;
-        simgrid::s4u::Mailbox *notify_mailbox;
+        simgrid::s4u::BarrierPtr barrier;
     };
 
 
@@ -147,21 +136,18 @@ namespace wrench {
     void SMPIExecutor::performBcast(std::vector<simgrid::s4u::Host *> &hosts,
                                     simgrid::s4u::Host *root_host,
                                     int data_size) {
-        // Create a mailbox to receive notifications of completion
-        auto mailbox = S4U_Mailbox::getTemporaryMailbox();
 
+        auto barrier = simgrid::s4u::Barrier::create(hosts.size() + 1);
         // Make sure that the root_host is the first host in the list of hosts, so that it's always rank 0
         auto it = std::find(hosts.begin(), hosts.end(), root_host);
         std::iter_swap(hosts.begin(), it);
 
         // Start actors to do an MPI_BCast
         SMPI_app_instance_start(("MPI_Bcast_" + std::to_string(simgrid::s4u::this_actor::get_pid())).c_str(),
-                                MPI_Bcast_partipant(data_size, mailbox),
+                                MPI_Bcast_partipant(data_size, barrier),
                                 hosts);
-        // Wait for all of those actors to be done
-        for (int i=0; i < hosts.size(); i++) {
-            S4U_Mailbox::getMessage(mailbox);
-        }
+
+        barrier->wait();
     }
 
 }
