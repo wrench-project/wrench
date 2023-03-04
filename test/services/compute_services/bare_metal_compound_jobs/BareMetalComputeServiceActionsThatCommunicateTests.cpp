0410899d5d0e15f359e3d02960f0fd9533994062
Henri Casanova <henric@hawaii.edu>
2023-03-03 14:52:29 -1000

    Test++ bug--

--- test/services/compute_services/bare_metal_compound_jobs/BareMetalComputeServiceActionsThatCommunicateTests.cpp
+++ test/services/compute_services/bare_metal_compound_jobs/BareMetalComputeServiceActionsThatCommunicateTests.cpp
@@ -253,9 +253,19 @@ private:
           communicator->MPI_Alltoall(10000000);
           WRENCH_INFO("Done with the Alltoall");
 
+          communicator->barrier();
+
+          WRENCH_INFO("Doing an Alltoall");
+          communicator->MPI_Alltoall(10000000, "mpich");
+          WRENCH_INFO("Done with the Alltoall");
+
+          communicator->barrier();
+
           WRENCH_INFO("Doing a Bcast");
           communicator->MPI_Bcast(0, 10000000);
           WRENCH_INFO("Done with the Bcast");
+
+          communicator->barrier();
 
           WRENCH_INFO("Doing a Barrier");
           communicator->MPI_Barrier();
