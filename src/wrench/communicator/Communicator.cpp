f4c9a29a839c48b0fff16d4e3ba0a5ebccfdadb2
Henri Casanova <henric@hawaii.edu>
2023-03-05 09:34:40 -1000

    removed useless include

--- src/wrench/communicator/Communicator.cpp
+++ src/wrench/communicator/Communicator.cpp
@@ -6,7 +6,6 @@
 #include "wrench/simulation//Simulation.h"
 #include "wrench/simgrid_S4U_util/S4U_Mailbox.h"
 #include "wrench/simgrid_S4U_util/S4U_PendingCommunication.h"
-#include "smpi/smpi.h"
 #include "simgrid/s4u.hpp"
 
 WRENCH_LOG_CATEGORY(wrench_core_communicator, "Log category for Communicator");
@@ -22,7 +21,6 @@ namespace wrench {
             S4U_Mailbox::retireTemporaryMailbox(item.second);
         }
     }
-
 
     /**
      * @brief Factory method to construct a communicator
