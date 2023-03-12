9539f5f3790995bd207d80f5f90d3fee537465bf
Henri Casanova <henric@hawaii.edu>
2023-03-11 19:10:10 -1000

    Tst fix

--- test/services/compute_services/batch_standard_and_pilot_jobs/BatchServiceBatschedQueueWaitTimePredictionTest.cpp
+++ test/services/compute_services/batch_standard_and_pilot_jobs/BatchServiceBatschedQueueWaitTimePredictionTest.cpp
@@ -733,7 +733,7 @@ private:
 
             std::map<std::string, std::string> batch_job_args1;
             batch_job_args1["-N"] = "2";
-            batch_job_args["-t"] = "300";//time in seconds
+            batch_job_args1["-t"] = "300";//time in seconds
             batch_job_args1["-c"] = "4";//number of cores per node
             try {
                 job_manager->submitJob(job1, this->test->compute_service, batch_job_args1);
@@ -761,7 +761,7 @@ private:
 
             std::map<std::string, std::string> batch_job_args2;
             batch_job_args2["-N"] = "4";
-            batch_job_args["-t"] = "300";//time in seconds
+            batch_job_args2["-t"] = "300";//time in seconds
             batch_job_args2["-c"] = "4";//number of cores per node
             try {
                 job_manager->submitJob(job2, this->test->compute_service, batch_job_args2);
