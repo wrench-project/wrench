ad92a98c88d3aeac465ac0491214cede80a1348f
Riley <rkam9@hawaii.edu>
2023-05-19 15:29:40 -1000

    Changes to SimulatorController.cpp

--- tools/wrench/wrench-daemon/include/callback-map.h
+++ tools/wrench/wrench-daemon/include/callback-map.h
@@ -19,6 +19,7 @@ request_handlers["createStandardJob"] = [sc](json data) { return sc->createStand
 request_handlers["submit"] = [sc](json data) { return sc->submitStandardJob(std::move(data)); };
 request_handlers["addBareMetalComputeService"] = [sc](json data) { return sc->addBareMetalComputeService(std::move(data)); };
 request_handlers["addCloudComputeService"] = [sc](json data) { return sc->addCloudComputeService(std::move(data)); };
+request_handlers["supportsCompoundJobs"] = [sc](json data) { return sc->supportsCompoundJobs(std::move(data)); };
 request_handlers["addSimpleStorageService"] = [sc](json data) { return sc->addSimpleStorageService(std::move(data)); };
 request_handlers["addFileRegistryService"] = [sc](json data) { return sc->addFileRegistryService(std::move(data)); };
 request_handlers["createFileCopy"] = [sc](json data) { return sc->createFileCopyAtStorageService(std::move(data)); };
