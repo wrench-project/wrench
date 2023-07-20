aa46f69801921c7cf887f26e994c423eba2f418f
Henri Casanova <henric@hawaii.edu>
2023-07-20 01:08:48 -1000

    Preparing release 2.2

--- examples/workflow_api/real-workflow-example/SimpleWorkflowSimulator.cpp
+++ examples/workflow_api/real-workflow-example/SimpleWorkflowSimulator.cpp
@@ -13,9 +13,6 @@
 #include "SimpleWMS.h"
 #include <wrench/tools/wfcommons/WfCommonsWorkflowParser.h>
 
-static bool ends_with(const std::string &str, const std::string &suffix) {
-    return str.size() >= suffix.size() && 0 == str.compare(str.size() - suffix.size(), suffix.size(), suffix);
-}
 
 /**
  * @brief An example that demonstrate how to run a simulation of a simple Workflow
