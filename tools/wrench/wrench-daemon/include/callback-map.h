31fd7f04a90b96963603a690517f2f66a0a30d8e
Sukaryo Roy Heilscher <sukaryor@hawaii.edu>
2024-02-27 12:30:35 -1000

    Implementation of FileRegistry: RemoveEntry

--- tools/wrench/wrench-daemon/include/callback-map.h
+++ tools/wrench/wrench-daemon/include/callback-map.h
@@ -29,6 +29,7 @@ request_handlers["supportsStandardJobs"] = [sc](json data) { return sc->supports
 request_handlers["addSimpleStorageService"] = [sc](json data) { return sc->addSimpleStorageService(std::move(data)); };
 request_handlers["addFileRegistryService"] = [sc](json data) { return sc->addFileRegistryService(std::move(data)); };
 request_handlers["addEntry"] = [sc](json data) { return sc->fileRegistryServiceAddEntry(std::move(data)); };
+request_handlers["removeEntry"] = [sc](json data) { return sc->fileRegistryServiceRemoveEntry(std::move(data)); };
 request_handlers["createFileCopy"] = [sc](json data) { return sc->createFileCopyAtStorageService(std::move(data)); };
 request_handlers["lookupFile"] = [sc](json data) { return sc->lookupFileAtStorageService(std::move(data)); };
 request_handlers["createVM"] = [sc](json data) { return sc->createVM(std::move(data)); };
