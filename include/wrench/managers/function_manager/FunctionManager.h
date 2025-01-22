1d36f9a3d9dcbb527b3124ed0e7598434934b8f4
Victor Pagan <vicpagan808@gmail.com>
2025-01-22 00:24:10 -1000

    Created FunctionManager skeletons

---
+++ include/wrench/managers/function_manager/FunctionManager.h
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2017. The WRENCH Team.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+
+#ifndef WRENCH_FUNCTIONMANAGER_H
+#define WRENCH_FUNCTIONMANAGER_H
+
+#include "wrench/services/Service.h"
+
+
+namespace wrench {
+
+    class FunctionManager : public Service {
+        public:
+
+        protected:
+        
+        private:
+    }
+
+}// namespace wrench
+
+#endif//WRENCH_FUNCTIONMANAGER_H
