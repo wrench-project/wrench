313e899a01b32dc2c032666adaa40eb324a25828
Victor Pagan <vicpagan808@gmail.com>
2025-01-29 00:36:07 -1000

    Basic FunctionManager implementation
    Created based FunctionManager methods based on JobManager.cpp
    
    TODO: implement serverless function specific failure causes and message cases

--- src/wrench/managers/function_manager/FunctionManagerMessage.cpp
+++ src/wrench/managers/function_manager/FunctionManagerMessage.cpp
@@ -19,4 +19,47 @@ namespace wrench {
     FunctionManagerMessage::FunctionManagerMessage() : SimulationMessage(0) {
     }
 
+    /**
+     * @brief Constructor 
+     * @param function: the function that is invoked
+     * @param sl_compute_service: the ServerlessComputeService on which it ran 
+     */
+    FunctionManagerFunctionCompletedMessage::FunctionManagerFunctionCompletedMessage(std::shared_ptr<Function> function,
+                                                                                     std::shared_ptr<ServerlessComputeService> sl_compute_service
+        : FunctionManagerMessage() {
+#ifdef WRENCH_INTERNAL_EXCEPTIONS
+        if ((function == nullptr) || (sl_compute_service == nullptr)) {
+            throw std::invalid_argument("FunctionManagerFunctionCompletedMessage::FunctionManagerFunctionCompletedMessage(): Invalid arguments");
+        }
+#endif
+        this->function = std::move(function);
+        this->sl_compute_service = std::move(sl_compute_service);
+    }
+
+    /**
+     * @brief Constructor 
+     * @param function: the function that is invoked
+     * @param sl_compute_service: the ServerlessComputeService on which it ran 
+     * @param cause: the cause of the failure
+     */
+    FunctionManagerFunctionFailedMessage::FunctionManagerFunctionFailedMessage(std::shared_ptr<Function> function,
+                                                                               std::shared_ptr<ServerlessComputeService> sl_compute_service,
+                                                                               std::shared_ptr<FailureCause> cause) : FunctionManagerMessage() {
+#ifdef WRENCH_INTERNAL_EXCEPTIONS
+        if ((function == nullptr) || (sl_compute_service == nullptr) || (cause == nullptr)) {
+            throw std::invalid_argument("FunctionManagerFunctionFailedMessage::FunctionManagerFunctionFailedMessage(): Invalid arguments");
+        }
+#endif
+        this->function = std::move(function);
+        this->sl_compute_service = std::move(sl_compute_service);
+        this->cause = std::move(cause);
+    }
+
+    /**
+     * @brief Message sent to the job manager to wake it up
+     */
+    FunctionManagerWakeupMessage::FunctionManagerWakeupMessage() : FunctionManagerMessage() {
+    }
+
+
 }// namespace wrench
