99d20b38149b047290ac9e70faa5bf37f43f0785
Henri Casanova <henric@hawaii.edu>
2025-02-21 11:00:13 -1000

    Many updates to make the image download feature working on the server side

---
+++ include/wrench/services/compute/serverless/ServerlessComputeServiceProperty.h
@@ -0,0 +1,30 @@
+/**
+* Copyright (c) 2017. The WRENCH Team.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+
+#ifndef WRENCH_SERVERLESSCOMPUTESERVICEPROPERTY_H
+#define WRENCH_SERVERLESSCOMPUTESERVICEPROPERTY_H
+
+#include <map>
+
+#include "wrench/services/compute/ComputeServiceProperty.h"
+
+namespace wrench {
+
+    /**
+    * @brief Configurable properties for a bare_metal_standard_jobs
+    */
+    class ServerlessComputeServiceProperty : public ComputeServiceProperty {
+
+    public:
+    };
+
+}// namespace wrench
+
+#endif//WRENCH_SERVERLESSCOMPUTESERVICEPROPERTY_H
