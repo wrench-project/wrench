e14f6e54101ce7f07565efa154b5194eafdae414
Henri Casanova <henric@hawaii.edu>
2025-09-30 12:00:20 -1000

    Drafted a complete example of custom events and messages

--- examples/action_api/batch-multi-controller/BatchServiceController.cpp
+++ examples/action_api/batch-multi-controller/BatchServiceController.cpp
@@ -12,9 +12,10 @@
  ** service or delegate jobs to a peer...
  **/
 
-#include <iostream>
-
 #include "BatchServiceController.h"
+#include "JobGenerationController.h"
+
+#include "ControlMessages.h"
 
 WRENCH_LOG_CATEGORY(batch_service_controller, "Log category for BatchServiceController");
 
@@ -42,6 +43,9 @@ namespace wrench {
                     Simulation::getHostName().c_str(),
                     Simulation::getCurrentSimulatedDate());
 
+        // Create my job manager
+        _job_manager = this->createJobManager();
+
         while (true) {
             this->waitForAndProcessNextEvent();
         }
@@ -49,12 +53,41 @@ namespace wrench {
 
     void BatchServiceController::processEventCustom(const std::shared_ptr<CustomEvent> &event) {
 
+        if (auto job_request_message = std::dynamic_pointer_cast<JobRequestMessage>(event->message)) {
+            WRENCH_INFO("Received a job request message for job %s: %d compute nodes for %d seconds",
+                job_request_message->_name.c_str(), job_request_message->_num_compute_nodes,
+                job_request_message->_runtime);
+            // Decide whether to forward or not based on some random criterion
+            if (job_request_message->_can_forward && (job_request_message->_num_compute_nodes % 2)) {
+                // Forward the job
+                WRENCH_INFO("Forwarding the job to my peer");
+                this->_peer->commport->dputMessage(
+                    new JobRequestMessage(
+                        job_request_message->_name,
+                        job_request_message->_num_compute_nodes,
+                        job_request_message->_runtime,
+                        false));
+            } else {
+                // Do the job myself
+                WRENCH_INFO("Submitting the job to my own batch service");
+                auto job = _job_manager->createCompoundJob(job_request_message->_name);
+                job->addSleepAction("", job_request_message->_runtime);
+                job->addSleepAction("", job_request_message->_runtime);
+                std::map<string, string> job_args;
+                job_args["-N"] = std::to_string(job_request_message->_num_compute_nodes);
+                job_args["-t"] = std::to_string(job_request_message->_runtime);
+                job_args["-c"] = "10";
+                _job_manager->submitJob(job, _batch_compute_service, job_args);
+            }
+        }
 
     }
 
 
     void BatchServiceController::processEventCompoundJobCompletion(const std::shared_ptr<CompoundJobCompletedEvent> &event) {
-
+        auto job_name = event->job->getName();
+        WRENCH_INFO("Notified that job %s, which I ran locally, has completed. Sending notification...", job_name.c_str());
+        _originator->commport->dputMessage(new JobNotificationMessage(job_name));
     }
 
 }// namespace wrench
