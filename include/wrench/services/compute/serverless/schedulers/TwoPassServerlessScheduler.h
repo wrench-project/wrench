729c87d351b6a24a72dcaade49df1e6dae612083
Henri Casanova <henric@hawaii.edu>
2026-08-12 21:48:15 -1000

    Scheduling Code cleanup/abstraction/refactor

--- include/wrench/services/compute/serverless/schedulers/TwoPassServerlessScheduler.h
+++ include/wrench/services/compute/serverless/schedulers/TwoPassServerlessScheduler.h
@@ -23,16 +23,36 @@ namespace wrench {
             const ServerlessStateOfTheSystem* state) override;
 
     protected:
+        /**
+         * @brief Sort compute nodes for an invocation, where the first compute nodes
+         *        are considered first when making scheduling decisions for this invocation
+         * @param state the state of the system
+         * @param invocation an invocation
+         * @return a sorted list of compute nodes
+         */
         virtual std::vector<std::shared_ptr<ServerlessComputeNode>> sortComputeNodesForInvocation(
             const ServerlessStateOfTheSystem* state,
             const std::shared_ptr<Invocation>& invocation) = 0;
 
+        /**
+         * @brief Sort compute nodes, with the first compute nodes considered first
+         *        when making scheduling decisions
+         * @param state the state of the system
+         * @return a sorted list of ServerlessComputeNodes
+         */
         virtual std::vector<std::shared_ptr<ServerlessComputeNode>> sortComputeNodes(
             const ServerlessStateOfTheSystem* state) = 0;
 
+        /**
+         * @brief Sort schedulable invocations, where the first invocations are considered
+         *        first when making scheduling decisions
+         * @param state the state of the system
+         * @param invocations the list of schedulable invocation
+         * @return a sort list of invocations
+         */
         virtual std::vector<std::shared_ptr<Invocation>> sortSchedulableInvocations(
             const ServerlessStateOfTheSystem* state,
-            const std::vector<std::shared_ptr<Invocation>>& invocation) = 0;
+            const std::vector<std::shared_ptr<Invocation>>& invocations) = 0;
 
     private:
         virtual void makeImageDecisions(const std::shared_ptr<ServerlessSchedulingDecisions>& decisions,
